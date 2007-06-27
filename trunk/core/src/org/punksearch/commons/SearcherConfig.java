d5fe3c416d1d0536c3a5faf43d4b667eb2a34ad6
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-06-27 10:43:50 +0000

    Initial import.

---
+++ trunk/core/src/org/punksearch/commons/SearcherConfig.java
@@ -0,0 +1,183 @@
+package org.punksearch.commons;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.punksearch.ip.IpRange;
+
+
+/**
+ * Class for storing indexer config
+ */
+public class SearcherConfig
+{
+	private static SearcherConfig	singleton;
+
+	private String					indexDirectory;
+	private int						indexThreads	= 1;
+	private int						indexDeep		= 5;
+	private String					smbDomain		= "";
+	private String					smbUser			= "";
+	private String					smbPassword		= "";
+	private int						smbTimeout		= 5000;
+	private int						maxClauseCount  = 1000000;
+	private List<IpRange>			ipRanges		= new ArrayList<IpRange>();
+	private String					ftpDefaultEnc   = "";
+	private Map<String, String>     ftpCustomEnc    = new HashMap<String, String>();
+
+	private SearcherConfig()
+	{
+	}
+
+	public void setIndexDirectory(String indexDirectory)
+	{
+		this.indexDirectory = indexDirectory;
+	}
+
+	public String getIndexDirectory()
+	{
+		return indexDirectory;
+	}
+
+	public void setIndexThreads(int indexThreads)
+	{
+		this.indexThreads = indexThreads;
+	}
+
+	public int getIndexThreads()
+	{
+		return indexThreads;
+	}
+
+	public void setIndexDeep(int indexDeep)
+	{
+		this.indexDeep = indexDeep;
+	}
+
+	public int getIndexDeep()
+	{
+		return indexDeep;
+	}
+
+	public void setIpRanges(String rangesString)
+	{
+		ipRanges.clear();
+		String[] rangeChunks = rangesString.split(",");
+		for (String chunk : rangeChunks)
+		{
+			ipRanges.add(new IpRange(chunk));
+		}
+	}
+
+	public List<IpRange> getIpRanges()
+	{
+		return ipRanges;
+	}
+
+	public String getIpRangesString()
+	{
+		String ranges = "";
+		for (IpRange range : ipRanges)
+		{
+			ranges += "," + range.toString();
+		}
+		return (ranges.length() != 0)? ranges.substring(1) : "";
+	}
+	
+	public void setSmbLogin(String smbLogin)
+	{
+		if (smbLogin.length() != 0 && smbLogin.contains("\\") && smbLogin.contains(":"))
+		{
+			this.smbDomain = smbLogin.substring(0, smbLogin.indexOf("\\"));
+			this.smbUser = smbLogin.substring(smbLogin.indexOf("\\") + 1, smbLogin.indexOf(":"));
+			this.smbPassword = smbLogin.substring(smbLogin.indexOf(":") + 1);
+		}
+		else
+		{
+			this.smbDomain = "";
+			this.smbUser = "";
+			this.smbPassword = "";
+		}
+	}
+
+	public String getSmbLogin()
+	{
+		String result = "";
+		if (getSmbDomain().length() != 0)
+			result += getSmbDomain() + "\\";
+		if (getSmbUser().length() != 0)
+			result += getSmbUser() + ":" + getSmbPassword();
+		return result;
+	}
+
+	public String getSmbDomain()
+	{
+		return smbDomain;
+	}
+
+	public String getSmbUser()
+	{
+		return smbUser;
+	}
+
+	public String getSmbPassword()
+	{
+		return smbPassword;
+	}
+
+	public static SearcherConfig getInstance()
+	{
+		if (null == singleton)
+			singleton = new SearcherConfig();
+		return singleton;
+	}
+
+	public int getSmbTimeout()
+	{
+		return smbTimeout;
+	}
+
+	public void setSmbTimeout(int timeout)
+	{
+		this.smbTimeout = timeout;
+	}
+	
+	public int getMaxClauseCount()
+	{
+		return maxClauseCount;
+	}
+
+	public void setMaxClauseCount(int maxClauseCount)
+	{
+		this.maxClauseCount = maxClauseCount;
+	}
+
+	public String getFtpDefaultEncoding()
+	{
+		return ftpDefaultEnc;
+	}
+
+	public void setFtpDefaultEncoding(String ftpDefaultEnc)
+	{
+		this.ftpDefaultEnc = ftpDefaultEnc;
+	}
+	
+	public void setFtpCustomEncodings(String encString)
+	{
+		if (encString == null || encString.length() == 0) return;
+		
+		String[] chunks = encString.split(",");
+		for (String chunk : chunks)
+		{
+			String[] parts = chunk.split(":");
+			ftpCustomEnc.put(parts[0], parts[1]);
+		}
+	}
+	
+	public Map<String, String> getFtpCustomEncodings()
+	{
+		return ftpCustomEnc;
+	}
+}
---
+++ trunk/core/src/org/punksearch/indexer/IndexerThread.java
@@ -0,0 +1,99 @@
+/**
+ * IndexerThread.java
+ * Created on 25.06.2006
+ * Author     Evgeny Shiriaev
+ * Email      arpmipg@gmail.com
+ */
+
+package org.punksearch.indexer;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.logging.Logger;
+
+import jcifs.smb.SmbException;
+
+import org.punksearch.commons.SearcherException;
+
+
+/**
+ * Class represents indexer thread
+ */
+public class IndexerThread extends Thread
+{
+	private Logger __log = Logger.getLogger(IndexerThread.class.getName());
+
+	private String ip;
+	
+	public IndexerThread(String name)
+	{
+		super(name);
+	}
+	
+	/**
+	 * Indexes net (smb and ftp)
+	 */
+	public void run()
+	{
+		while ((ip = Indexer.getInstance().nextIp()) != null)
+		{
+			try
+			{
+				__log.info(ip + " start indexing");
+				
+				IndexerOperator.getInstance().deleteDocuments(ip, "smb");
+				
+				try
+				{
+					SmbIndexer smbIndexer = new SmbIndexer(ip);
+					long sizeSmb = smbIndexer.index();
+					if (sizeSmb > 0)
+					{
+						__log.info("SMB: " + ip + " indexed: " + sizeSmb + " bytes");
+					}
+				}
+				catch (SmbException e)
+				{
+					String reason = (e.getRootCause() != null)? e.getRootCause().getMessage() : e.getMessage();
+					
+					//String reason = (e.getCause() == null)? "unknown reason" : e.getCause().getMessage();
+					__log.info("Indexing of smb host " + ip + " failed due to: " + reason);
+				}
+				
+				FtpIndexer ftpIndexer = new FtpIndexer(ip);
+				//String ftpHost = "ftp://" + ip;
+				//__log.debug(ftpHost + " is indexing");
+				IndexerOperator.getInstance().deleteDocuments(ip, "ftp");
+				long sizeFtp = ftpIndexer.index();
+				if (sizeFtp > 0)
+				{
+					__log.info("FTP: " + ip + " indexed: " + sizeFtp + " bytes");
+				}
+				
+				//IndexerOperator.getInstance().optimizeIndex();
+				IndexerOperator.getInstance().flushIndex();
+			}
+			catch (IllegalArgumentException e)
+			{
+				__log.warning("IAE: " + e);
+			}
+			catch (MalformedURLException e)
+			{
+				__log.warning("MUE: " + e);
+			}
+			catch (SearcherException e)
+			{
+				__log.warning("SE: " + e);
+			}
+			catch (IOException e)
+			{
+				__log.warning("Can't flush index! "+e.getMessage());
+			}
+		}
+	}
+	
+	public String getIp()
+	{
+		return ip;
+	}
+}
