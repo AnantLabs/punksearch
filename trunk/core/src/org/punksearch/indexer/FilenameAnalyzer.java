d61a89bbcf232e5cc70943c7a8813f394c8d1c27
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-07-28 00:03:42 +0000

    new classes for filenames and paths tokenization during indexing

---
+++ trunk/core/src/org/punksearch/indexer/FilenameAnalyzer.java
@@ -0,0 +1,20 @@
+package org.punksearch.indexer;
+
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LengthFilter;
+import org.apache.lucene.analysis.TokenStream;
+
+public class FilenameAnalyzer extends Analyzer
+{
+
+	@Override
+	public TokenStream tokenStream(String fieldName, Reader reader)
+	{
+	    TokenStream result = new FilenameTokenizer(reader);
+	    result = new LengthFilter(result, 3, 1000);
+	    return result;
+	}
+
+}
