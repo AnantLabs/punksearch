cbfdb5e3291bac2e97955e11e87c5f8bacd756b4
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-07-28 12:24:06 +0000

    some refactorings

--- trunk/web/src/org/punksearch/web/SearchParams.java
+++ trunk/web/src/org/punksearch/web/SearchParams.java
@@ -4,127 +4,99 @@ import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 
-public class SearchParams 
+public class SearchParams
 {
-	private static Logger __log	= Logger.getLogger(SearchParams.class.getName());
-	
-	public HttpServletRequest request = null;
-		
-	public String type = "everything";
-	public String query = null;
-	
-	public String dir = null;	
-	public String file = null;
-	public String ext = null;
+	private static Logger		__log		= Logger.getLogger(SearchParams.class.getName());
 
-	public Long minSize =  null;
-	public Long maxSize = null;
-	
-	public Long fromDate = null;
-	public Long toDate = null;
+	public String				type		= "everything";
+	public String				query		= null;
 
-	public Boolean ftp = true;
-	public Boolean smb = true;
-	
-	public Integer first = 0;
-	public Integer last = SearchPager.PAGE_SIZE-1;
-		
+	public String				dir			= null;
+	public String				file		= null;
+	public String				ext			= null;
+
+	public Long					minSize		= null;
+	public Long					maxSize		= null;
+
+	public Long					fromDate	= null;
+	public Long					toDate		= null;
+
+	//public Boolean				ftp			= true;
+	//public Boolean				smb			= true;
+
+	public Integer				first		= 0;
+	public Integer				last		= SearchPager.PAGE_SIZE - 1;
+
 	public SearchParams(HttpServletRequest request)
 	{
-		this.request = request;
-	
-		String typeParam = request.getParameter("type");
-		if (typeParam != null) this.type = typeParam;
-		
+		this.type = getStringValue(request, "type");
+		if (this.type.length() == 0)
+			this.type = "everything";
+
 		/* first & last */
-		try
-		{
-			this.first = Integer.valueOf(getStringValue("first"));
-		}
-		catch (NumberFormatException nfe)
-		{
-			__log.warning(nfe.getMessage());
-		}
-		try
-		{
-			this.last = Integer.valueOf(getStringValue("last"));
-		}
-		catch (NumberFormatException nfe)
-		{
-			__log.warning(nfe.getMessage());
-		}		
+		this.first = getIntegerValue(request, "first", 0);
+		this.last  = getIntegerValue(request, "last", SearchPager.PAGE_SIZE - 1);
 
 		if (type.equals("advanced"))
 		{
-			/* dir & file & ext*/		
-			this.dir = getStringValue("dir");
-			this.file = getStringValue("file");
-			this.ext = getStringValue("ext");
-			
+			/* dir & file & ext*/
+			this.dir = getStringValue(request, "dir");
+			this.file = getStringValue(request, "file");
+			this.ext = getStringValue(request, "ext");
+
 			/* maxSize & minSize */
-			try
-			{			
-				this.maxSize = (Long)Math.round(Double.valueOf(getStringValue("maxSize"))*1024*1024);
-			}
-			catch (NumberFormatException nfe)
-			{
-				__log.warning(nfe.getMessage());
-			}		
-			try
-			{
-				this.minSize = (Long)Math.round(Double.valueOf(getStringValue("minSize"))*1024*1024);
-			}
-			catch (NumberFormatException nfe)
-			{
-				__log.warning(nfe.getMessage());
-			}
-			if (this.minSize != null && this.maxSize != null && this.minSize > this.minSize)
+			this.minSize = (Long) Math.round(getDoubleValue(request, "minSize", 0.0) * 1024 * 1024);
+			this.maxSize = (Long) Math.round(getDoubleValue(request, "maxSize", 0.0) * 1024 * 1024);
+			if (minSize == 0) minSize = null;
+			if (maxSize == 0) maxSize = null;
+			if (this.minSize != null && this.maxSize != null && this.minSize > this.maxSize)
 			{
 				this.minSize = this.maxSize = null;
 			}
-					
+
 			/* fromDate & toDate */
 			DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 			try
 			{
-				this.fromDate = fmt.parse(getStringValue("fromDate")).getTime();
+				this.fromDate = fmt.parse(getStringValue(request, "fromDate")).getTime();
 			}
-			catch (ParseException pe) 
+			catch (ParseException pe)
 			{
-				__log.warning(pe.getMessage());
+				//__log.warning(pe.getMessage());
 			}
 			try
 			{
-				this.toDate = fmt.parse(getStringValue("toDate")).getTime();
+				this.toDate = fmt.parse(getStringValue(request, "toDate")).getTime();
 			}
-			catch (ParseException pe) 
+			catch (ParseException pe)
 			{
-				__log.warning(pe.getMessage());
+				//__log.warning(pe.getMessage());
 			}
 			if (this.fromDate != null && this.toDate != null && this.fromDate > this.toDate)
 			{
 				this.fromDate = this.toDate = null;
-			}		
-						
+			}
+
 			/* ftp & smb */
-			this.smb = (request.getParameter("smb") != null);
-			this.ftp = (request.getParameter("ftp") != null);	
+			//this.smb = (request.getParameter("smb") != null);
+			//this.ftp = (request.getParameter("ftp") != null);
 		}
 		else
 		{
-			this.query = getStringValue("query");			
-		}				
+			this.query = getStringValue(request, "query");
+		}
 	}
-	
-	public String getStringValue(String paramName)
+
+	public static String getStringValue(HttpServletRequest request, String paramName)
 	{
-		String paramValue = this.request.getParameter(paramName);
-		if (paramValue == null) return "";
-		
+		String paramValue = request.getParameter(paramName);
+		if (paramValue == null)
+			return "";
+
 		try
 		{
 			paramValue = new String(paramValue.getBytes("ISO-8859-1"), "UTF-8");
@@ -133,9 +105,28 @@ public class SearchParams
 		{
 			__log.warning(uee.getMessage());
 		}
-		
+
 		return paramValue;
 	}
+
+	public static Integer getIntegerValue(HttpServletRequest request, String paramName, Integer byDefault)
+	{
+		String paramValue = request.getParameter(paramName);
+		if (paramValue == null || paramValue.equals(""))
+			return byDefault;
+
+		return Integer.valueOf(paramValue);
+
+	}
 	
+	public static Double getDoubleValue(HttpServletRequest request, String paramName, Double byDefault)
+	{
+		String paramValue = request.getParameter(paramName);
+		if (paramValue == null || paramValue.equals(""))
+			return byDefault;
+		
+		return Double.valueOf(paramValue);
+		
+	}
 
 }
