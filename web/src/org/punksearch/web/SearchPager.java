fdf45fea3200637f0032ca9b4032722e538a37a9
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-07-28 12:24:06 +0000

    some refactorings

--- web/src/org/punksearch/web/SearchPager.java
+++ web/src/org/punksearch/web/SearchPager.java
@@ -1,22 +1,18 @@
 package org.punksearch.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class SearchPager {
 
 	private static final int PAGE_LINKS_PER_SIDE = 10;
 	public static final int PAGE_SIZE = 15;
 
-	private SearchParams params = null;
 	private int pageCount = 0;
 	
-	public SearchPager(SearchParams params)
-	{
-		this.params = params;
-	}
-
-    public String makePagesRow(int all)
+    public String makePagesRow(HttpServletRequest request, int all)
     {
     	pageCount = (all%PAGE_SIZE == 0)? all/PAGE_SIZE : all/PAGE_SIZE + 1; 
-		int cur = params.first/PAGE_SIZE;
+		int cur = SearchParams.getIntegerValue(request, "first", 0)/PAGE_SIZE;
     	
     	if (all <= PAGE_SIZE)
     		return "";
@@ -29,12 +25,12 @@ public class SearchPager {
     	int stopNum  = (cur + PAGE_LINKS_PER_SIDE + 1 > maxNum)? maxNum : cur + PAGE_LINKS_PER_SIDE + 1;
     	
     	String urlPrefix = "?";
-    	for (Object o_key : params.request.getParameterMap().keySet())
+    	for (Object o_key : request.getParameterMap().keySet())
     	{
     		String key = (String) o_key;
     		if (!key.equals("first") && !key.equals("last"))
     		{
-    				urlPrefix += key + "=" + params.getStringValue(key) + "&";
+    				urlPrefix += key + "=" + SearchParams.getStringValue(request, key) + "&";
     		}
     	}
     	
