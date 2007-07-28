bc9127d6ea62d460e81acca11e4993ed6ad3f63e
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-07-28 12:34:42 +0000

    use of index tokenization + fastSearch flag utilization

--- web/WebContent/search.jsp
+++ web/WebContent/search.jsp
@@ -1,18 +1,15 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ page import="java.util.List" %>
-<%@ page import="org.punksearch.web.SearchParams" %>
-<%@ page import="org.punksearch.web.SearchResult" %>
-<%@ page import="org.punksearch.web.SearchAction" %>
-<%@ page import="org.punksearch.web.SearchPager" %>
-
-<%! 
-	boolean showScores = false;
-%>
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<%@page import="org.punksearch.web.filters.TypeFilters"%>
-<%@page import="org.punksearch.commons.SearcherConfig"%>
 <html xmlns="http://www.w3.org/1999/xhtml">
+	<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+	<%@ page import="java.util.List" %>
+	<%@ page import="org.punksearch.web.SearchParams" %>
+	<%@ page import="org.punksearch.web.SearchResult" %>
+	<%@ page import="org.punksearch.web.SearchAction" %>
+	<%@ page import="org.punksearch.web.SearchPager" %>
+	<%@ page import="org.punksearch.web.filters.TypeFilters" %>
+	<%@ page import="org.punksearch.commons.SearcherConfig" %>
+	<%@ page import="java.util.Map" %>
+	<%! boolean showScores = false; %>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>PUNK LAN Search</title>
@@ -23,7 +20,6 @@
 				var params = "?type="+type;
 				var queryElement = document.forms["searchForm"].elements["query"];
 				var queryParam = ((queryElement==null)||(queryElement.value.length==0)) ? "" : "&query=" + queryElement.value;				
-				if (type == "advanced") params+="&ftp=on&smb=on"; else params+=queryParam;
 				window.location = params;
 			}
 			function validateQuery()
@@ -59,7 +55,7 @@
 			{			
 				if (tab[1].equals(params.type))
 				{
-					%><td class="tab selected"><%= tab[0] %></d><%		
+					%><td class="tab selected"><%= tab[0] %></td><%		
 				}
 				else
 				{
@@ -104,23 +100,19 @@
 				<tr>
 					<td>
 						date<span style="vertical-align: sub; font-size: 12px;">yyyy-mm-dd</span><br/>
-						<input type="text" id="fromDate" name="fromDate" value="<%= params.getStringValue("fromDate") %>" size="11" style="font-size:16px; font-weight:bold"/>&nbsp;-
-						<input type="text" id="toDate" name="toDate" value="<%= params.getStringValue("toDate") %>" size="11" style="font-size:16px; font-weight:bold"/>						
+						<input type="text" id="fromDate" name="fromDate" value="<%= SearchParams.getStringValue(request, "fromDate") %>" size="11" style="font-size:16px; font-weight:bold"/>&nbsp;-
+						<input type="text" id="toDate" name="toDate" value="<%= SearchParams.getStringValue(request, "toDate") %>" size="11" style="font-size:16px; font-weight:bold"/>						
 					</td>
 					<td>
 						size<span style="vertical-align: sub; font-size: 12px;">Mb</span><br/>
-						<input type="text" id="minSize" name="minSize" value="<%= params.getStringValue("minSize") %>" size="6" style="font-size:16px; font-weight:bold"/>&nbsp;-
-						<input type="text" id="maxSize" name="maxSize" value="<%= params.getStringValue("maxSize") %>" size="6" style="font-size:16px; font-weight:bold"/>						
+						<input type="text" id="minSize" name="minSize" value="<%= SearchParams.getStringValue(request, "minSize") %>" size="6" style="font-size:16px; font-weight:bold"/>&nbsp;-
+						<input type="text" id="maxSize" name="maxSize" value="<%= SearchParams.getStringValue(request, "maxSize") %>" size="6" style="font-size:16px; font-weight:bold"/>						
 					</td>
 					<td>
 						<input type="submit" value="search"/>
 					</td>						
 				</tr>				
 				<!--tr>
-					<td>
-						ftp<input type="checkbox" id="ftp" name="ftp" <%= params.ftp ? "checked" : "" %> style="border:0px;"/>
-						<input type="checkbox" id="smb" name="smb" <%= params.smb ? "checked" : "" %> style="border:0px;"/>smb
-					</td>
 					<td>
 						Hint<span style="color:white; font-weight:normal"> use "!" to negate search terms</span>				
 					</td>
@@ -141,63 +133,64 @@
 
 <div id="searchResultsContainer">
 	<%
-		if (!request.getParameterMap().isEmpty())
+		Map parameterMap = request.getParameterMap();
+		if (!parameterMap.isEmpty() && !(parameterMap.size() == 1 && parameterMap.get("type") != null))
 		{	
-		SearchAction searchAction  = new SearchAction(params);
-		List<SearchResult> searchResults = searchAction.doSearch();
-		if (searchResults != null)
-		{
-			if(!searchResults.isEmpty())
+			SearchAction searchAction  = new SearchAction(params);
+			List<SearchResult> searchResults = searchAction.doSearch();
+			if (searchResults != null)
 			{
-				SearchPager searchPager  = new SearchPager(params);
-				int overallCount = searchAction.getOverallCount();
-			 	String pageNums = searchPager.makePagesRow(overallCount);
-				%>
-				<table id="pager" cellspacing="0" cellpadding="0" align="center">
-					<tr>
-						<td style="font-size: 10pt;">
-							<span style="font-size: 14pt;"><%= overallCount %></span> items (<%= searchPager.getPageCount() %> pages) in <%= searchAction.getSearchTime()/1000.0 %> secs
-						</td>
-						<td style="text-align: right; vertical-align: bottom;"><%= pageNums %></td>
-					</tr>
-				</table>
-				<table id="results" cellspacing="0" align="center">
-				<%								
-					for (SearchResult file : searchResults)
-					{
+				if(!searchResults.isEmpty())
+				{
+					SearchPager searchPager  = new SearchPager();
+					int overallCount = searchAction.getOverallCount();
+				 	String pageNums = searchPager.makePagesRow(request, overallCount);
 					%>
-					<tr>
-						<td style="width: 16px; padding-right: 2px; vertical-align: top; padding-top: 4px;">
-							<img src="images/<%= (file.ext.length() != 0)? "stock_new-16.png" : "stock_folder-16.png" %>"/>
-						</td>
-						<td style="padding-left: 2px;">
-							<span style="font-size: 12pt;"><%= file.name %></span><%= (showScores)? "(" + file.score + ")": "" %><br/>
-							<span style="font-size: 8pt; color:#0070AD; padding-left: 0pt;"><%= file.host + "/" + file.path %></span>
-						</td>
-						<td style="text-align: right;"><%= file.date %></td>
-						<td style="text-align: right;"><%= file.size %> Mb</td>
-					</tr>
-					<%
-					}
-				%>
-				</table>
-			<%	
+					<table id="pager" cellspacing="0" cellpadding="0" align="center">
+						<tr>
+							<td style="font-size: 10pt;">
+								<span style="font-size: 14pt;"><%= overallCount %></span> items (<%= searchPager.getPageCount() %> pages) in <%= searchAction.getSearchTime()/1000.0 %> secs
+							</td>
+							<td style="text-align: right; vertical-align: bottom;"><%= pageNums %></td>
+						</tr>
+					</table>
+					<table id="results" cellspacing="0" align="center">
+					<%								
+						for (SearchResult file : searchResults)
+						{
+						%>
+						<tr>
+							<td style="width: 16px; padding-right: 2px; vertical-align: top; padding-top: 4px;">
+								<img src="images/<%= (file.ext.length() != 0)? "stock_new-16.png" : "stock_folder-16.png" %>"/>
+							</td>
+							<td style="padding-left: 2px;">
+								<span style="font-size: 12pt;"><%= file.name %></span><%= (showScores)? "(" + file.score + ")": "" %><br/>
+								<span style="font-size: 8pt; color:#0070AD; padding-left: 0pt;"><%= file.host + "/" + file.path %></span>
+							</td>
+							<td style="text-align: right;"><%= file.date %></td>
+							<td style="text-align: right;"><%= file.size %> Mb</td>
+						</tr>
+						<%
+						}
+					%>
+					</table>
+				<%	
+				}
+				else
+				{
+					%><div class="infoMessage">Search yields no results</div><%	
+				}
 			}
-			else
-			{
-				%><div class="infoMessage">Search yields no results</div><%	
-			}
-		}
 		}
 	%>
 </div>
 <% if (SearcherConfig.getInstance().getGoogleAnalyticsId().length() > 0) { %>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "<%= SearcherConfig.getInstance().getGoogleAnalyticsId() %>";
-urchinTracker();
-</script>
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "<%= SearcherConfig.getInstance().getGoogleAnalyticsId() %>";
+	urchinTracker();
+	</script>
 <% } %>
 </body>
 </html>
--- web/src/org/punksearch/web/SearchAction.java
+++ web/src/org/punksearch/web/SearchAction.java
@@ -1,87 +1,94 @@
 package org.punksearch.web;
 
-import java.util.LinkedList;
+import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
-import java.io.IOException;
 
-import org.apache.lucene.index.*;
-import org.apache.lucene.search.*;
 import org.apache.lucene.document.Document;
-
-import org.punksearch.searcher.*;
-import org.punksearch.searcher.filters.*;
-import org.punksearch.web.filters.TypeFilters;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Filter;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.WildcardQuery;
 import org.punksearch.commons.SearcherConfig;
 import org.punksearch.commons.SearcherConstants;
 import org.punksearch.commons.SearcherException;
+import org.punksearch.searcher.LuceneSearcher;
+import org.punksearch.searcher.filters.CompositeFilter;
+import org.punksearch.searcher.filters.FilterFactory;
+import org.punksearch.searcher.filters.NumberRangeFilter;
+import org.punksearch.web.filters.TypeFilters;
 
-import java.text.DecimalFormat;
-import java.text.DateFormat;
+public class SearchAction
+{
 
-public class SearchAction {
-	
-	private static final int MIN_TERM_LENGTH = 3;
-	
-	private static Logger __log	= Logger.getLogger(SearchParams.class.getName());
-	
-	private SearchParams params;	
-	private SearcherConfig config = SearcherConfig.getInstance();
-	
-	private long searchTime = 0;
-	private int overallCount = 0;
+	private static final int	MIN_TERM_LENGTH	= 3;
+
+	private static Logger		__log			= Logger.getLogger(SearchParams.class.getName());
+
+	private SearchParams		params;
+	private SearcherConfig		config			= SearcherConfig.getInstance();
+
+	private long				searchTime		= 0;
+	private int					overallCount	= 0;
 
 	public SearchAction(SearchParams params)
 	{
 		this.params = params;
 	}
-	
+
 	public List<SearchResult> doSearch()
 	{
 		Query query = null;
 		Filter filter = null;
-		
+
 		if (params.type.equals("advanced"))
 		{
 			query = makeAdvancedQuery();
-			
-			NumberRangeFilter sizeFilter = null;
+
+			NumberRangeFilter<Long> sizeFilter = null;
 			if (params.minSize != null || params.maxSize != null)
 			{
 				sizeFilter = FilterFactory.createNumberFilter(SearcherConstants.SIZE, params.minSize, params.maxSize);
 			}
-			
-			NumberRangeFilter dateFilter = null;
+
+			NumberRangeFilter<Long> dateFilter = null;
 			if (params.fromDate != null || params.toDate != null)
 			{
 				dateFilter = FilterFactory.createNumberFilter(SearcherConstants.DATE, params.fromDate, params.toDate);
 			}
-			
+
 			if (sizeFilter != null || dateFilter != null)
 			{
 				CompositeFilter resultFilter = new CompositeFilter();
-				if (sizeFilter != null) resultFilter.add(sizeFilter);
-				if (dateFilter != null) resultFilter.add(dateFilter);
+				if (sizeFilter != null)
+					resultFilter.add(sizeFilter);
+				if (dateFilter != null)
+					resultFilter.add(dateFilter);
 				filter = resultFilter;
-			}			
+			}
 		}
 		else
 		{
 			query = makeSimpleQuery();
 			filter = TypeFilters.get(params.type);
 		}
-					
+
 		List<SearchResult> searchResults = null;
-		
+
 		if (query != null)
 		{
+			__log.info("query constructed: " + query.toString());
 			try
 			{
-				LuceneSearcher searcher = new LuceneSearcher(config.getIndexDirectory());				
+				LuceneSearcher searcher = new LuceneSearcher(config.getIndexDirectory());
 				Date startDate = new Date();
-				List<Document> results  = searcher.search(query, params.first, params.last, filter);
-				Date stopDate  = new Date();
+				List<Document> results = searcher.search(query, params.first, params.last, filter);
+				Date stopDate = new Date();
 				searchTime = stopDate.getTime() - startDate.getTime();
 				overallCount = searcher.overallCount();
 				searchResults = prepareResults(results);
@@ -94,12 +101,12 @@ public class SearchAction {
 			{
 				__log.warning(se.getMessage());
 			}
-			
+
 		}
-		
+
 		return searchResults;
 	}
-	
+
 	public int getOverallCount()
 	{
 		return overallCount;
@@ -107,80 +114,79 @@ public class SearchAction {
 
 	public long getSearchTime()
 	{
-		return searchTime; 
+		return searchTime;
 	}
-	
-    private static List<String> prepareQueryParameter(String str)
-    {
-    	List<String> result = new LinkedList<String>();
-    	if (str != null)
-    	{
-	    	String[] terms = str.toLowerCase().split(" ");
-	    	for (String term : terms)
-	    	{
-	    		term = term.replace("*", "");
-	    		term = term.trim();
-	    		if (term.length() >= MIN_TERM_LENGTH)
-	    		{
-	    			result.add(term);
-	    		}
-	    	}
-    	}
-    	return result;
-    }
-    
-    private static List<SearchResult> prepareResults(List<Document> results)
-    {
-    	if (results == null) 
-    		return null;
-    	
-    	List<SearchResult> searchResults = new LinkedList<SearchResult>();
-		for (Document doc: results)
+
+	private static List<String> prepareQueryParameter(String str)
+	{
+		List<String> result = new LinkedList<String>();
+		if (str != null)
+		{
+			str = str.replaceAll("\\*|_|-|\\.|,|\\:|\\[|\\]|#|\\(|\\)|'|/|&", " ");
+			String[] terms = str.toLowerCase().split(" ");
+			for (String term : terms)
+			{
+				term = term.trim();
+				if (term.length() >= MIN_TERM_LENGTH)
+				{
+					result.add(term);
+				}
+			}
+		}
+		return result;
+	}
+
+	private static List<SearchResult> prepareResults(List<Document> results)
+	{
+		if (results == null)
+			return null;
+
+		List<SearchResult> searchResults = new LinkedList<SearchResult>();
+		for (Document doc : results)
 		{
 			searchResults.add(new SearchResult(doc));
-		}		
-    	return searchResults;
-    }
-        
+		}
+		return searchResults;
+	}
 
-    private Query makeSimpleQuery()
-    {
+	private Query makeSimpleQuery()
+	{
 		BooleanQuery query = new BooleanQuery(false);
 		BooleanQuery.setMaxClauseCount(config.getMaxClauseCount());
 
-		List<String> terms 	= prepareQueryParameter(params.query);
-		
+		List<String> terms = prepareQueryParameter(params.query);
+
 		for (String item : terms)
 		{
 			BooleanQuery itemQuery = new BooleanQuery();
-			
+
 			BooleanClause.Occur occurItem = BooleanClause.Occur.MUST;
 			if (item.startsWith("!"))
 			{
 				item = item.substring(1);
 				occurItem = BooleanClause.Occur.MUST_NOT;
 			}
-			
-			Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, "*" + item + "*"));
+
+			Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, prepareItem(item)));
 			itemQuery.add(nameQuery, BooleanClause.Occur.SHOULD);
-			
-			Query pathQuery = new WildcardQuery(new Term(SearcherConstants.PATH, "*" + item + "*"));
+
+			Query pathQuery = new WildcardQuery(new Term(SearcherConstants.PATH, prepareItem(item)));
 			itemQuery.add(pathQuery, BooleanClause.Occur.SHOULD);
-			
+
 			query.add(itemQuery, occurItem);
 		}
-		
+
 		return query;
-    }
-    
-    private Query makeAdvancedQuery()
-    {
+	}
+
+	private Query makeAdvancedQuery()
+	{
 		BooleanQuery query = new BooleanQuery(false);
 		BooleanQuery.setMaxClauseCount(config.getMaxClauseCount());
 
-		List<String> dirTerms 	= prepareQueryParameter(params.dir);
-		List<String> fileTerms	= prepareQueryParameter(params.file);
-		List<String> extTerms	= prepareQueryParameter(params.ext);
+		List<String> dirTerms = prepareQueryParameter(params.dir);
+		List<String> fileTerms = prepareQueryParameter(params.file);
+		List<String> extTerms = prepareQueryParameter(params.ext);
 
 		if (fileTerms.size() != 0 || extTerms.size() != 0) // search for files
 		{
@@ -189,26 +195,18 @@ public class SearchAction {
 				BooleanQuery fileQuery = new BooleanQuery();
 				for (String item : fileTerms)
 				{
-					BooleanQuery itemQuery = new BooleanQuery();
-					
 					BooleanClause.Occur occurItem = BooleanClause.Occur.SHOULD;
 					if (item.startsWith("!"))
 					{
 						item = item.substring(1);
 						occurItem = BooleanClause.Occur.MUST_NOT;
 					}
-					
-					Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, "*" + item + "*"));
-					itemQuery.add(nameQuery, BooleanClause.Occur.MUST);
-					
-					Query extensionQuery = new TermQuery(new Term(SearcherConstants.EXTENSION, SearcherConstants.DIRECTORY_EXTENSION));
-					itemQuery.add(extensionQuery, BooleanClause.Occur.MUST_NOT);
-					
-					fileQuery.add(itemQuery, occurItem);
+					Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, prepareItem(item)));
+					fileQuery.add(nameQuery, occurItem);
 				}
 				query.add(fileQuery, BooleanClause.Occur.MUST);
 			}
-			
+
 			if (extTerms.size() != 0)
 			{
 				BooleanQuery extQuery = new BooleanQuery();
@@ -219,23 +217,28 @@ public class SearchAction {
 				}
 				query.add(extQuery, BooleanClause.Occur.MUST);
 			}
-			
+			else
+			{
+				Query extensionQuery = new TermQuery(new Term(SearcherConstants.EXTENSION, SearcherConstants.DIRECTORY_EXTENSION));
+				query.add(extensionQuery, BooleanClause.Occur.MUST_NOT);
+			}
+
 			if (dirTerms.size() != 0) // restrict files to occur in specified directories only
 			{
 				BooleanQuery dirQuery = new BooleanQuery();
 				int negations = 0;
-				
+
 				for (String item : dirTerms)
-				{				
+				{
 					BooleanClause.Occur occurItem = BooleanClause.Occur.SHOULD;
 					if (item.startsWith("!"))
 					{
 						item = item.substring(1);
 						occurItem = BooleanClause.Occur.MUST_NOT;
-						negations++;						
+						negations++;
 					}
-					
-					Query pathQuery = new WildcardQuery(new Term(SearcherConstants.PATH, "*" + item + "*"));
+
+					Query pathQuery = new WildcardQuery(new Term(SearcherConstants.PATH, prepareItem(item)));
 					dirQuery.add(pathQuery, occurItem);
 				}
 				if (dirTerms.size() == negations) // it must be at least one positive clause in query to be executed. so add one if all user clauses are nagative.
@@ -249,22 +252,22 @@ public class SearchAction {
 		else if (dirTerms.size() != 0) // search for directories only, since file name was not specified
 		{
 			for (String item : dirTerms)
-			{				
+			{
 				BooleanQuery dirQuery = new BooleanQuery();
-				
+
 				BooleanClause.Occur occurItem = BooleanClause.Occur.SHOULD;
 				if (item.startsWith("!"))
 				{
 					item = item.substring(1);
 					occurItem = BooleanClause.Occur.MUST_NOT;
 				}
-				
-				Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, "*" + item + "*"));
+
+				Query nameQuery = new WildcardQuery(new Term(SearcherConstants.NAME, prepareItem(item)));
 				dirQuery.add(nameQuery, BooleanClause.Occur.MUST);
-				
+
 				Query extensionQuery = new TermQuery(new Term(SearcherConstants.EXTENSION, SearcherConstants.DIRECTORY_EXTENSION));
 				dirQuery.add(extensionQuery, BooleanClause.Occur.MUST);
-				
+
 				query.add(dirQuery, occurItem);
 			}
 		}
@@ -273,6 +276,11 @@ public class SearchAction {
 			return null;
 		}
 		return query;
-    }
+	}
+
+	private String prepareItem(String item)
+	{
+		return (config.isFastSearch()) ? item + "*" : "*" + item + "*";
+	}
 
 }
