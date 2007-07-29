843f5bc15bdeefde8284d19955def5a408d39d43
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-07-29 19:04:41 +0000

    some new file types added, small refactoring

--- web/src/org/punksearch/web/filters/TypeFilters.java
+++ web/src/org/punksearch/web/filters/TypeFilters.java
@@ -4,37 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Filter;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryFilter;
+import org.apache.lucene.search.TermQuery;
 import org.punksearch.commons.SearcherConstants;
 import org.punksearch.searcher.filters.CompositeFilter;
 import org.punksearch.searcher.filters.FilterFactory;
 import org.punksearch.searcher.filters.NumberRangeFilter;
+import org.punksearch.web.Types;
 
 public class TypeFilters
 {
-	public static String	TYPE_FILM	= "film";
-	public static String	TYPE_MUSIC	= "music";
-	public static String	TYPE_ISO	= "iso";
 
-	private static Long		MB			= 1024L * 1024L;
+	private static Long					MB		= 1024L * 1024L;
 
-	private static Map<String, Filter> filters = new HashMap<String, Filter>();
-	
+	private static Map<String, Filter>	filters	= new HashMap<String, Filter>();
+
 	static
 	{
 		init();
 	}
-	
+
 	public static void reset()
 	{
 		filters.clear();
 		init();
 	}
-	
+
 	public static Filter get(String type)
 	{
 		return filters.get(type);
@@ -42,31 +42,63 @@ public class TypeFilters
 
 	private static void init()
 	{
-		filters.put(TYPE_FILM,  new CachedFilter(createFilter(TYPE_FILM)));
-		filters.put(TYPE_MUSIC, new CachedFilter(createFilter(TYPE_MUSIC)));
-		filters.put(TYPE_ISO,   new CachedFilter(createFilter(TYPE_ISO)));
+		filters.put(Types.FILM,    new CachedFilter(createFilter(Types.FILM)));
+		filters.put(Types.CLIP,    new CachedFilter(createFilter(Types.CLIP)));
+		filters.put(Types.MUSIC,   new CachedFilter(createFilter(Types.MUSIC)));
+		filters.put(Types.ISO,     new CachedFilter(createFilter(Types.ISO)));
+		filters.put(Types.PICTURE, new CachedFilter(createFilter(Types.PICTURE)));
+		filters.put(Types.ARCHIVE, new CachedFilter(createFilter(Types.ARCHIVE)));
+		filters.put(Types.EXE,     new CachedFilter(createFilter(Types.EXE)));
+		filters.put(Types.DOC,     new CachedFilter(createFilter(Types.DOC)));
+		filters.put(Types.DIR,     new CachedFilter(createFilter(Types.DIR)));
 	}
-	
+
 	private static Filter createFilter(String type)
 	{
 		Long min = null;
 		Long max = null;
 		String ext = null;
-		if (type.equals(TYPE_FILM))
+		if (type.equals(Types.FILM))
 		{
 			min = 500 * MB;
 			ext = "avi vob mpg";
 		}
-		else if (type.equals(TYPE_MUSIC))
+		else if (type.equals(Types.CLIP))
+		{
+			min = 3 * MB;
+			max = 100 * MB;
+			ext = "avi mpg mpeg wmv mov wmf";
+		}
+		else if (type.equals(Types.MUSIC))
 		{
 			min = 1 * MB;
 			max = 100 * MB;
-			ext = "mp3 ogg wav flac";
+			ext = "mp3 ogg wav wma flac";
 		}
-		else if (type.equals(TYPE_ISO))
+		else if (type.equals(Types.ISO))
 		{
 			min = 10 * MB;
 			ext = "iso mdf";
+		}
+		else if (type.equals(Types.PICTURE))
+		{
+			ext = "jpg jpeg gif png bmp tif";
+		}
+		else if (type.equals(Types.ARCHIVE))
+		{
+			ext = "zip rar arj gz tgz tar bz2";
+		}
+		else if (type.equals(Types.EXE))
+		{
+			ext = "exe";
+		}
+		else if (type.equals(Types.DOC))
+		{
+			ext = "doc xls ppt pdf ps djvu odt txt htm html xhtml";
+		}
+		else if (type.equals(Types.DIR))
+		{
+			ext = "";
 		}
 		else
 		{
@@ -81,11 +113,20 @@ public class TypeFilters
 			filter.add(sizeFilter);
 		}
 
-		if (ext != null && ext.length() != 0)
+		if (ext != null)
 		{
 			try
 			{
-				Query extQuery = new QueryParser(SearcherConstants.EXTENSION, new StandardAnalyzer()).parse(ext);
+				Query extQuery;
+				if (ext.length() != 0)
+				{
+					extQuery = new QueryParser(SearcherConstants.EXTENSION, new StandardAnalyzer()).parse(ext);
+				}
+				else
+				{
+					extQuery = new TermQuery(new Term(SearcherConstants.EXTENSION, ""));
+				}
+				System.out.println(extQuery);
 				QueryFilter extFilter = new QueryFilter(extQuery);
 				filter.add(extFilter);
 			}
@@ -98,5 +139,5 @@ public class TypeFilters
 		return filter;
 
 	}
-	
+
 }
