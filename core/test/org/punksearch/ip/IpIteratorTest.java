36c3ec578180ad86f45517fd57a287d9a5323374
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-11-24 13:46:48 +0000

    2 more test methods

--- core/test/org/punksearch/ip/IpIteratorTest.java
+++ core/test/org/punksearch/ip/IpIteratorTest.java
@@ -3,10 +3,6 @@ package org.punksearch.ip;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.punksearch.ip.Ip;
-import org.punksearch.ip.IpIterator;
-import org.punksearch.ip.IpRange;
-
 import junit.framework.TestCase;
 
 public class IpIteratorTest extends TestCase
@@ -45,5 +41,32 @@ public class IpIteratorTest extends TestCase
 		Ip ip2 = it.next();
 		assertEquals("1.2.4.0", ip2.toString());
 		assertTrue(it.hasNext());
+	}
+	
+	public void testMultiRange() {
+		List<IpRange> ranges = new ArrayList<IpRange>();
+		ranges.add(new IpRange("1.2.3.1-1.2.3.100"));
+		ranges.add(new IpRange("1.2.30.10-1.2.30.200"));
+		IpIterator it = new IpIterator(ranges);
+		Ip ip = it.next();
+		while (!ip.toString().equals("1.2.3.100")) {
+			ip = it.next();
+		}
+		assertTrue(it.hasNext());
+		ip = it.next();
+		assertEquals("1.2.30.10", ip.toString());
+		assertTrue(it.hasNext());
+	}
+	
+	public void testUnsupportedRemove() {
+		List<IpRange> ranges = new ArrayList<IpRange>();
+		ranges.add(new IpRange("1.2.3.255-1.2.4.10"));
+		IpIterator it = new IpIterator(ranges);
+		Ip ip = it.next();
+		try {
+			it.remove();
+			fail("Should fail since remove() is not supported");
+		} catch (UnsupportedOperationException success) {
+		}
 	}
 }
