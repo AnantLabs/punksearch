268b98fb04c3d56b93580411b0ab749cdf1885ef
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2007-12-18 23:43:00 +0000

    filter result documents by chunks

--- core/src/org/punksearch/searcher/ResultFilter.java
+++ core/src/org/punksearch/searcher/ResultFilter.java
@@ -1,9 +1,12 @@
 package org.punksearch.searcher;
 
+import java.util.List;
+
 import org.apache.lucene.document.Document;
 
 public interface ResultFilter {
 
 	public boolean matches(Document doc);
 	
+	public List<Integer> filter(List<Document> docs);
 }
