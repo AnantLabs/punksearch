a77a4da029cf9701eba100fe472e7ce021513303
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-01-09 21:01:04 +0000

    refactored. port numbers are extracted to constants; 139 smb port is old and can be manually enabled, but disabled by default; timeout increased to 1000

--- core/src/org/punksearch/commons/OnlineChecker.java
+++ core/src/org/punksearch/commons/OnlineChecker.java
@@ -6,43 +6,42 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public class OnlineChecker {
-	
+
 	// TODO: extract to settings
-	private static final int TIMEOUT = 500;
-	
-	public static boolean isOnline(String host)
-	{
-		if ( !(host.startsWith("smb") || host.startsWith("ftp")) )
-		{
+	private static final int     TIMEOUT          = 1000;
+	private static final boolean USE_OLD_SMB_PORT = false;
+
+	private static final int     SMB_PORT         = 445;
+	private static final int     SMB_OLD_PORT     = 139;
+	private static final int     FTP_PORT         = 21;
+
+	public static boolean isOnline(String host) {
+		if (!(host.startsWith("smb") || host.startsWith("ftp"))) {
 			throw new IllegalArgumentException("Unknown protocol: " + host);
 		}
-		
-		int ipStart = (host.indexOf(":") == 3)? 6 : 4; // cut smb_ or smb://
+
+		int ipStart = (host.indexOf(":") == 3) ? 6 : 4; // cut smb_ or smb://
 		String ip = host.substring(ipStart);
-		
-		return (host.startsWith("ftp"))? isActiveFtp(ip) : isActiveSmb(ip);
+
+		return (host.startsWith("ftp")) ? isActiveFtp(ip) : isActiveSmb(ip);
 	}
-	
+
 	public static boolean isActiveSmb(String ip) {
-		return isActive(ip, 445) || isActive(ip, 139);
+		return isActive(ip, SMB_PORT) || (USE_OLD_SMB_PORT && isActive(ip, SMB_OLD_PORT));
 	}
-	
+
 	public static boolean isActiveFtp(String ip) {
-		return isActive(ip, 21);
+		return isActive(ip, FTP_PORT);
 	}
-	
-	public static boolean isActive(String ip, int port)
-	{
-		try
-		{
+
+	public static boolean isActive(String ip, int port) {
+		try {
 			SocketAddress sockaddr = new InetSocketAddress(ip, port);
 			Socket s = new Socket();
 			s.connect(sockaddr, TIMEOUT);
 			s.close();
 			return true;
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			return false;
 		}
 	}
