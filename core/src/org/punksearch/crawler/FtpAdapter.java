3946f9f325579ce94d2cdfa21b18a8c5a00762ad
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-03-14 08:11:42 +0000

    new (refactored) crawler implementation

---
+++ core/src/org/punksearch/crawler/FtpAdapter.java
@@ -0,0 +1,210 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package org.punksearch.crawler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.punksearch.commons.OnlineChecker;
+import org.punksearch.commons.PunksearchProperties;
+
+import com.enterprisedt.net.ftp.FTPClient;
+import com.enterprisedt.net.ftp.FTPConnectMode;
+import com.enterprisedt.net.ftp.FTPException;
+import com.enterprisedt.net.ftp.FTPFile;
+
+/**
+ * @author Yury Soldak (ysoldak@gmail.com)
+ */
+public class FtpAdapter implements ProtocolAdapter {
+
+	private static Logger __log   = Logger.getLogger(FtpAdapter.class.getName());
+	
+	private FTPClient     ftp     = new FTPClient();
+
+	private String        rootPath;
+
+	public boolean connect(String ip) {
+		if (!OnlineChecker.isActiveFtp(ip)) {
+			return false;
+		}
+
+		try {
+			setupFtpClient(ip);
+			ftp.connect();
+			ftp.login(getUser(), getPassword());
+			ftp.keepAlive();
+			setRootPath(ftp.pwd());
+			return true;
+		} catch (Exception e) {
+			__log.info("ftp: Exception (" + e.getMessage() + ") during connecting the server: " + ip);
+			return false;
+		} finally {
+			disconnect();
+		}
+	}
+
+	public void disconnect() {
+		String ip = "unknown-ip";
+		try {
+			if (ftp.connected()) {
+				ip = ftp.getRemoteHost();
+				ftp.quit();
+			}
+		} catch (Exception e) {
+			__log.info("ftp: exception during disconnect from " + ip + ": " + e.getMessage());
+		}
+	}
+
+	/**
+	 * test-friendly method
+	 * @param path
+	 */
+	protected void setRootPath(String path) {
+		rootPath = path;
+	}
+	
+	public String getFullPath(Object item) {
+		return getPath(item) + "/" + getName(item);
+	}
+
+	public long getModificationTime(Object item) {
+		return ((FTPFile) item).lastModified().getTime();
+	}
+
+	public String getName(Object item) {
+		return ((FTPFile) item).getName();
+	}
+
+	public String getPath(Object item) {
+		String path = ((FTPFile) item).getPath();
+		return path.substring(rootPath.length());
+	}
+
+	public String getProtocol() {
+		return "ftp";
+	}
+
+	public Object getRootDir() {
+		if (ftp == null || !ftp.connected()) {
+			throw new IllegalStateException("can't get root dir since not connected to any ftp host");
+		}
+		try {
+			return ftp.fileDetails(rootPath);
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+	}
+
+	public long getSize(Object item) {
+		return ((FTPFile) item).size();
+	}
+
+	public boolean isDirectory(Object item) {
+		return ((FTPFile) item).isDir();
+	}
+
+	public boolean isFile(Object item) {
+		return (!((FTPFile) item).isDir() && !((FTPFile) item).isLink());
+	}
+
+	public boolean isHidden(Object item) {
+		return false;
+	}
+
+	public boolean isLink(Object item) {
+		return ((FTPFile) item).isLink();
+	}
+
+	public Object[] listFiles(Object dir) {
+		FTPFile res = (FTPFile) dir;
+		FTPFile[] items = {};
+		try {
+			items = ftp.dirDetails(res.getPath());
+		} catch (IOException e) {
+			// host communication problem occured, rethrow the exception so crawler will give up crawling this host
+			throw new RuntimeException(e);
+		} catch (Exception e) {
+			__log.info("ftp: Exception (" + e.getMessage() + ") during changing or listing directory: " + dir);
+		}
+		return items;
+	}
+
+	public Map<String, String> parseCustomEncodings(String encString) {
+		Map<String, String> result = new HashMap<String, String>();
+		if (encString == null || encString.length() == 0) {
+			return result;
+		}
+
+		String[] chunks = encString.split(",");
+		for (String chunk : chunks) {
+			String[] parts = chunk.split(":");
+			result.put(parts[0], parts[1]);
+		}
+		return result;
+	}
+
+	public Map<String, String> parseCustomModes(String modString) {
+		Map<String, String> result = new HashMap<String, String>();
+		if (modString == null || modString.length() == 0) {
+			return result;
+		}
+
+		String[] chunks = modString.split(",");
+		for (String chunk : chunks) {
+			String[] parts = chunk.split(":");
+			result.put(parts[0], parts[1]);
+		}
+		return result;
+	}
+
+	private String getFtpEncodingForIp(String ip) {
+		String defaultEnc = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.encoding.default");
+		String customEnc = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.encoding.custom");
+		Map<String, String> encMap = parseCustomEncodings(customEnc);
+		return (encMap.containsKey(ip)) ? encMap.get(ip) : defaultEnc;
+	}
+
+	private boolean isActiveModeForIp(String ip) {
+		String defaultMode = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.mode.default");
+		String customMode = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.mode.custom");
+		Map<String, String> customModes = parseCustomModes(customMode);
+		String modeStr = (customModes.containsKey(ip)) ? customModes.get(ip) : defaultMode;
+		return (modeStr.equals("active"));
+	}
+
+	private void setupFtpClient(String ip) throws FTPException, IOException {
+		if (ftp == null) {
+			ftp = new FTPClient();
+		}
+		ftp.setControlEncoding(getFtpEncodingForIp(ip));
+		if (isActiveModeForIp(ip)) {
+			ftp.setConnectMode(FTPConnectMode.ACTIVE);
+		} else {
+			ftp.setConnectMode(FTPConnectMode.PASV);
+		}
+		ftp.setRemoteHost(ip);
+		ftp.setTimeout(Integer.parseInt(PunksearchProperties.getProperty("org.punksearch.crawler.ftp.timeout")));
+	}
+	
+	private String getUser() {
+		String user = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.user");
+		return (user.length() == 0)? "anonymous" : user;
+	}
+	
+	private String getPassword() {
+		String passwd = PunksearchProperties.getProperty("org.punksearch.crawler.ftp.password");
+		return (passwd.length() == 0)? "some@email.com" : passwd;
+	}
+
+}
---
+++ core/src/org/punksearch/crawler/NetworkCrawler.java
@@ -0,0 +1,98 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package org.punksearch.crawler;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.punksearch.commons.FileTypes;
+import org.punksearch.commons.PunksearchProperties;
+import org.punksearch.indexer.IndexOperator;
+import org.punksearch.indexer.IpIteratorWrapper;
+import org.punksearch.ip.IpRange;
+
+/**
+ * @author Yury Soldak (ysoldak@gmail.com)
+ */
+public class NetworkCrawler implements Runnable {
+	private static Logger     __log      = Logger.getLogger(NetworkCrawler.class.getName());
+
+	private FileTypes         fileTypes  = new FileTypes();
+	private String            indexDirectory;
+
+	private List<HostCrawler> threadList = new ArrayList<HostCrawler>();
+
+	public NetworkCrawler(String indexDir) {
+		String file = NetworkCrawler.class.getClassLoader().getResource("standard.types").getFile();
+		File standardTypes = new File(file);
+		fileTypes.readFromFile(standardTypes);
+		this.indexDirectory = indexDir;
+	}
+
+	public void run() {
+		String range = PunksearchProperties.getProperty("org.punksearch.crawler.range");
+		int threadCount = Integer.parseInt(PunksearchProperties.getProperty("org.punksearch.crawler.threads"));
+
+		IpIteratorWrapper iter = new IpIteratorWrapper(parseRanges(range));
+
+		threadList.clear();
+
+		try {
+			IndexOperator.init(indexDirectory);
+
+			long startTime = new Date().getTime();
+			__log.info("Indexing process started");
+
+			for (int i = 0; i < threadCount; i++) {
+				HostCrawler indexerThread = new HostCrawler("IndexerThread" + i, iter, fileTypes);
+				// indexerThread.setDaemon(true);
+				indexerThread.start();
+				threadList.add(indexerThread);
+			}
+			for (Thread indexerThread : threadList) {
+				indexerThread.join();
+			}
+
+			IndexOperator.getInstance().optimizeIndex();
+			IndexOperator.getInstance().flushIndex();
+
+			long finishTime = new Date().getTime();
+			__log.info("Index process is finished in " + ((finishTime - startTime) / 1000) + " sec");
+		} catch (Exception e) {
+			__log.warning("Indexer.run(): exception occured. " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			IndexOperator.close();
+		}
+	}
+
+	public void stop() {
+		for (Thread thread : threadList) {
+			thread.interrupt();
+		}
+	}
+
+	public List<HostCrawler> getThreads() {
+		return threadList;
+	}
+
+	public List<IpRange> parseRanges(String rangesString) {
+		List<IpRange> result = new ArrayList<IpRange>();
+		String[] rangeChunks = rangesString.split(",");
+		for (String chunk : rangeChunks) {
+			result.add(new IpRange(chunk));
+		}
+		return result;
+	}
+}
