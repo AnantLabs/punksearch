e9f4b0fd3a8b5e4f88da2ca60634a89610314e36
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-03-14 08:14:23 +0000

    crawler command-line interface

---
+++ core/src/org/punksearch/cli/CrawlerMain.java
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package org.punksearch.cli;
+
+import org.punksearch.commons.PunksearchProperties;
+import org.punksearch.crawler.NetworkCrawler;
+
+/**
+ * @author Yury Soldak (ysoldak@gmail.com)
+ *
+ */
+public class CrawlerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PunksearchProperties.setProperty("org.punksearch.crawler.range", args[0]);
+		NetworkCrawler crawler = new NetworkCrawler(PunksearchProperties.getProperty("org.punksearch.index.dir"));
+		crawler.run();
+	}
+
+}
