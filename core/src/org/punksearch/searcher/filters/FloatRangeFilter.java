ba15c5f5d972a36e3382122f50014ecd4a50ce95
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-03-31 14:29:34 +0000

    formatting

--- core/src/org/punksearch/searcher/filters/FloatRangeFilter.java
+++ core/src/org/punksearch/searcher/filters/FloatRangeFilter.java
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.searcher.filters;
 
 import java.io.IOException;
@@ -11,12 +21,12 @@ import org.apache.lucene.search.Filter;
 
 public class FloatRangeFilter extends Filter {
 
-	private String fieldName;
-	private Float lowerTerm = 0.0f;
-	private Float upperTerm = 0.0f;
+	private String  fieldName;
+	private Float   lowerTerm = 0.0f;
+	private Float   upperTerm = 0.0f;
 	private boolean includeLower;
-	private boolean includeUpper;	
-	
+	private boolean includeUpper;
+
 	public FloatRangeFilter(String fieldName, Float lowerTerm, Float upperTerm, boolean includeLower, boolean includeUpper) {
 		this.fieldName = fieldName;
 		this.lowerTerm = lowerTerm;
@@ -34,81 +44,81 @@ public class FloatRangeFilter extends Filter {
 			throw new IllegalArgumentException("The upper bound must be non-null to be inclusive");
 		}
 	}
-	
+
 	public BitSet bits(IndexReader reader) throws IOException {
-        BitSet bits = new BitSet(reader.maxDoc());
-        TermEnum enumerator = reader.terms(new Term(fieldName, ""));
-        
-        try {
-            
-            if (enumerator.term() == null) {
-                return bits;
-            }
-        
-            TermDocs termDocs = reader.termDocs();
-            try {
-                do {
-                    Term term = enumerator.term();
-                    if (term != null && term.field().equals(fieldName)) {
-                    	Float termFloat = Float.valueOf(term.text());
-                        if (lowerTerm == null || lowerTerm <  termFloat || (includeLower && lowerTerm == termFloat)) {
-                            if (upperTerm != null && (upperTerm < termFloat || (!includeUpper && upperTerm == termFloat))) {
-                                continue;
-                            }
-                            // we have a good term, find the docs
-                            termDocs.seek(enumerator.term());
-                            while (termDocs.next()) {
-                                bits.set(termDocs.doc());
-                            }
-                            
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                while (enumerator.next());
-                
-            } finally {
-                termDocs.close();
-            }
-        } finally {
-            enumerator.close();
-        }
-        
+		BitSet bits = new BitSet(reader.maxDoc());
+		TermEnum enumerator = reader.terms(new Term(fieldName, ""));
+
+		try {
+
+			if (enumerator.term() == null) {
+				return bits;
+			}
+
+			TermDocs termDocs = reader.termDocs();
+			try {
+				do {
+					Term term = enumerator.term();
+					if (term != null && term.field().equals(fieldName)) {
+						Float termFloat = Float.valueOf(term.text());
+						if (lowerTerm == null || lowerTerm < termFloat || (includeLower && lowerTerm == termFloat)) {
+							if (upperTerm != null && (upperTerm < termFloat || (!includeUpper && upperTerm == termFloat))) {
+								continue;
+							}
+							// we have a good term, find the docs
+							termDocs.seek(enumerator.term());
+							while (termDocs.next()) {
+								bits.set(termDocs.doc());
+							}
+
+						}
+					} else {
+						break;
+					}
+				} while (enumerator.next());
+
+			} finally {
+				termDocs.close();
+			}
+		} finally {
+			enumerator.close();
+		}
+
 		System.out.println(lowerTerm + " " + upperTerm);
 
-        return bits;
+		return bits;
 	}
 
-    public String toString() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(fieldName);
-        buffer.append(":");
-        buffer.append(includeLower ? "[" : "{");
-        if (null != lowerTerm) {
-            buffer.append(lowerTerm);
-        }
-        buffer.append("-");
-        if (null != upperTerm) {
-            buffer.append(upperTerm);
-        }
-        buffer.append(includeUpper ? "]" : "}");
-        return buffer.toString();
-    }
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(fieldName);
+		buffer.append(":");
+		buffer.append(includeLower ? "[" : "{");
+		if (null != lowerTerm) {
+			buffer.append(lowerTerm);
+		}
+		buffer.append("-");
+		if (null != upperTerm) {
+			buffer.append(upperTerm);
+		}
+		buffer.append(includeUpper ? "]" : "}");
+		return buffer.toString();
+	}
 
-    /** Returns true if <code>o</code> is equal to this. */
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FloatRangeFilter)) return false;
-        FloatRangeFilter other = (FloatRangeFilter) o;
+	/** Returns true if <code>o</code> is equal to this. */
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof FloatRangeFilter)) return false;
+		FloatRangeFilter other = (FloatRangeFilter) o;
 
-        if (!this.fieldName.equals(other.fieldName)
-            || this.includeLower != other.includeLower
-            || this.includeUpper != other.includeUpper
-           ) { return false; }
-        if (this.lowerTerm != null ? !this.lowerTerm.equals(other.lowerTerm) : other.lowerTerm != null) return false;
-        if (this.upperTerm != null ? !this.upperTerm.equals(other.upperTerm) : other.upperTerm != null) return false;
-        return true;
-    }	
-	
+		if (!this.fieldName.equals(other.fieldName) || this.includeLower != other.includeLower || this.includeUpper != other.includeUpper) {
+			return false;
+		}
+		if (this.lowerTerm != null ? !this.lowerTerm.equals(other.lowerTerm) : other.lowerTerm != null)
+		    return false;
+		if (this.upperTerm != null ? !this.upperTerm.equals(other.upperTerm) : other.upperTerm != null)
+		    return false;
+		return true;
+	}
+
 }
--- core/src/org/punksearch/searcher/filters/NumberRangeFilter.java
+++ core/src/org/punksearch/searcher/filters/NumberRangeFilter.java
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.searcher.filters;
 
 import java.io.IOException;
@@ -9,86 +19,65 @@ import org.apache.lucene.index.TermDocs;
 import org.apache.lucene.index.TermEnum;
 import org.apache.lucene.search.Filter;
 
-public abstract class NumberRangeFilter<N extends Comparable<N>> extends Filter
-{
-	private String	fieldName;
-	private N		lowerTerm;
-	private N		upperTerm;
-	private boolean	includeLower;
-	private boolean	includeUpper;
+public abstract class NumberRangeFilter<N extends Comparable<N>> extends Filter {
+	private String  fieldName;
+	private N       lowerTerm;
+	private N       upperTerm;
+	private boolean includeLower;
+	private boolean includeUpper;
 
-	public NumberRangeFilter(String fieldName, N lowerTerm, N upperTerm, boolean includeLower, boolean includeUpper)
-	{
+	public NumberRangeFilter(String fieldName, N lowerTerm, N upperTerm, boolean includeLower, boolean includeUpper) {
 		this.fieldName = fieldName;
 		this.lowerTerm = lowerTerm;
 		this.upperTerm = upperTerm;
 		this.includeLower = includeLower;
 		this.includeUpper = includeUpper;
 
-		if (null == lowerTerm && null == upperTerm)
-		{
+		if (null == lowerTerm && null == upperTerm) {
 			throw new IllegalArgumentException("At least one value must be non-null");
 		}
-		if (includeLower && null == lowerTerm)
-		{
+		if (includeLower && null == lowerTerm) {
 			throw new IllegalArgumentException("The lower bound must be non-null to be inclusive");
 		}
-		if (includeUpper && null == upperTerm)
-		{
+		if (includeUpper && null == upperTerm) {
 			throw new IllegalArgumentException("The upper bound must be non-null to be inclusive");
 		}
 	}
 
-	public BitSet bits(IndexReader reader) throws IOException
-	{
+	public BitSet bits(IndexReader reader) throws IOException {
 		BitSet bits = new BitSet(reader.maxDoc());
 		TermEnum enumerator = reader.terms(new Term(fieldName, ""));
 
-		try
-		{
+		try {
 
-			if (enumerator.term() == null)
-			{
+			if (enumerator.term() == null) {
 				return bits;
 			}
 
 			TermDocs termDocs = reader.termDocs();
-			try
-			{
-				do
-				{
+			try {
+				do {
 					Term term = enumerator.term();
-					if (term != null && term.field().equals(fieldName))
-					{
+					if (term != null && term.field().equals(fieldName)) {
 						N termValue = termTextToNumber(term.text());
-						if (lowerTerm == null || lowerTerm.compareTo(termValue) < 0 || (includeLower && lowerTerm.compareTo(termValue) == 0))
-						{
-							if (upperTerm == null || upperTerm.compareTo(termValue) > 0 || (includeUpper && upperTerm.compareTo(termValue) == 0))
-							{
+						if (lowerTerm == null || lowerTerm.compareTo(termValue) < 0 || (includeLower && lowerTerm.compareTo(termValue) == 0)) {
+							if (upperTerm == null || upperTerm.compareTo(termValue) > 0 || (includeUpper && upperTerm.compareTo(termValue) == 0)) {
 								// we have a good term, find the docs
 								termDocs.seek(enumerator.term());
-								while (termDocs.next())
-								{
+								while (termDocs.next()) {
 									bits.set(termDocs.doc());
 								}
 							}
 						}
-					}
-					else
-					{
+					} else {
 						break;
 					}
-				}
-				while (enumerator.next());
+				} while (enumerator.next());
 
-			}
-			finally
-			{
+			} finally {
 				termDocs.close();
 			}
-		}
-		finally
-		{
+		} finally {
 			enumerator.close();
 		}
 
@@ -97,19 +86,16 @@ public abstract class NumberRangeFilter<N extends Comparable<N>> extends Filter
 
 	public abstract N termTextToNumber(String text);
 
-	public String toString()
-	{
+	public String toString() {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append(fieldName);
 		buffer.append(":");
 		buffer.append(includeLower ? "[" : "{");
-		if (null != lowerTerm)
-		{
+		if (null != lowerTerm) {
 			buffer.append(lowerTerm);
 		}
 		buffer.append("-");
-		if (null != upperTerm)
-		{
+		if (null != upperTerm) {
 			buffer.append(upperTerm);
 		}
 		buffer.append(includeUpper ? "]" : "}");
@@ -117,22 +103,18 @@ public abstract class NumberRangeFilter<N extends Comparable<N>> extends Filter
 	}
 
 	/** Returns true if <code>o</code> is equal to this. */
-	public boolean equals(Object o)
-	{
-		if (this == o)
-			return true;
-		if (!(o instanceof NumberRangeFilter))
-			return false;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof NumberRangeFilter)) return false;
 		NumberRangeFilter other = (NumberRangeFilter) o;
 
-		if (!this.fieldName.equals(other.fieldName) || this.includeLower != other.includeLower || this.includeUpper != other.includeUpper)
-		{
+		if (!this.fieldName.equals(other.fieldName) || this.includeLower != other.includeLower || this.includeUpper != other.includeUpper) {
 			return false;
 		}
 		if (this.lowerTerm != null ? !this.lowerTerm.equals(other.lowerTerm) : other.lowerTerm != null)
-			return false;
+		    return false;
 		if (this.upperTerm != null ? !this.upperTerm.equals(other.upperTerm) : other.upperTerm != null)
-			return false;
+		    return false;
 		return true;
 	}
 
