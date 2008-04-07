c3c0d9573088dbf0f37b6a8a9b4d89f3e07183fa
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-07 13:11:06 +0000

    refactoring

--- trunk/builder/skel/crawler.sh
+++ trunk/builder/skel/crawler.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-CP=../etc
-for file in ../lib/*.jar; do
+CP=.
+for file in lib/*.jar; do
 	CP=$CP:$file
 done;
 
 echo $CP
 
-java -Xmx1024m -cp $CP org.punksearch.cli.CrawlerMain $1
+java -Xmx256m -cp $CP org.punksearch.cli.CrawlerMain $1
