97519712c0c7ec09a413c0b98407952ba70f5b46
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-07 11:44:45 +0000

    sync with core and GPL comment

--- web/src/org/punksearch/web/InitServlet.java
+++ web/src/org/punksearch/web/InitServlet.java
@@ -1,35 +1,41 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.web;
+
+import java.io.FileNotFoundException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-import org.punksearch.searcher.Searcher;
+import org.punksearch.common.PunksearchProperties;
 
-public class InitServlet extends HttpServlet
-{
-	public void init(ServletConfig config) throws ServletException
-	{
+public class InitServlet extends HttpServlet {
+
+	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		
-		SearcherConfig sc = SearcherConfig.getInstance();
-		sc.setIndexDirectory(getServletContext().getInitParameter("indexDirectory"));
-		sc.setIndexThreads(Integer.parseInt(getServletContext().getInitParameter("indexThreads")));
-		sc.setIndexDeep(Integer.parseInt(getServletContext().getInitParameter("indexDeep")));
-		sc.setIpRanges(getServletContext().getInitParameter("ipRange"));
-		sc.setSmbLogin(getServletContext().getInitParameter("smbLogin"));
-		sc.setFtpDefaultEncoding(getServletContext().getInitParameter("ftpDefaultEncoding"));
-		sc.setFtpCustomEncodings(getServletContext().getInitParameter("ftpCustomEncodings"));
-		sc.setFtpDefaultMode(getServletContext().getInitParameter("ftpDefaultMode"));
-		sc.setFtpCustomModes(getServletContext().getInitParameter("ftpCustomModes"));
-		sc.setFtpTimeout(Integer.parseInt(getServletContext().getInitParameter("ftpTimeout")));
-		sc.setMaxClauseCount(Integer.parseInt(getServletContext().getInitParameter("searchMaxClauseCount")));
-		sc.setGoogleAnalyticsId(getServletContext().getInitParameter("googleAnalyticsId"));
-		sc.setFastSearch(Boolean.parseBoolean(getServletContext().getInitParameter("fastSearch")));
-		
-		//System.setProperty("jcifs.smb.client.responseTimeout", "5000");
+
+		try {
+			String home = getServletContext().getInitParameter("punksearch_home");
+			if (home != null) {
+				System.setProperty("org.punksearch.home", home);
+			}
+			PunksearchProperties.loadDefault();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// System.setProperty("jcifs.smb.client.responseTimeout", "5000");
 		System.setProperty("jcifs.util.loglevel", "0");
-		
-		SearcherWrapper.init(SearcherConfig.getInstance().getIndexDirectory());
+
+		SearcherWrapper.init();
 	}
 }
