56f6933e316edbcfb6c564a12f833c65679607c5
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-07 11:39:20 +0000

    head comment about GPL

--- trunk/web/src/org/punksearch/web/statistics/FileTypeStatistics.java
+++ trunk/web/src/org/punksearch/web/statistics/FileTypeStatistics.java
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.web.statistics;
 
 import java.text.NumberFormat;
