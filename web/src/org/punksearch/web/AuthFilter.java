e4d69d82b37fd9946d75ef482ad446620e1a3030
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-07 01:53:54 +0000

    optimize imports

--- web/src/org/punksearch/web/AuthFilter.java
+++ web/src/org/punksearch/web/AuthFilter.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.punksearch.indexer.Indexer;
-
 public class AuthFilter implements Filter
 {
 	private static Logger	__log			= Logger.getLogger(AuthFilter.class.getName());
