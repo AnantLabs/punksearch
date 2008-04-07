abf1fe5b398425d091e81486f27d65e93c2a63a5
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-07 11:31:59 +0000

    javascript extracted

---
+++ trunk/web/WebContent/js/common.js
@@ -0,0 +1,12 @@
+function toggle(id) {
+	var obj = document.getElementById(id);
+	obj.style.display = (obj.style.display == "none")? "block" : "none";
+}
+
+function changeLogoColors()
+{
+	var color = document.getElementById("logo.punk").style.color;
+	document.getElementById("logo.punk").style.color = document.getElementById("logo.search").style.color;
+	document.getElementById("logo.search").style.color = color;
+}
+
