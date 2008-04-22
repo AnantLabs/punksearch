fc1d14c6720dc2e84c0bf02f0dd25cce0a7e0eef
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-04-22 23:05:33 +0000

    utility methods and comments and formatted

--- trunk/core/src/org/punksearch/ip/Ip.java
+++ trunk/core/src/org/punksearch/ip/Ip.java
@@ -1,11 +1,22 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.ip;
 
-public class Ip implements Comparable<Ip>
-{
-	private Long	value;
+public class Ip implements Comparable<Ip> {
+	private Long value;
 
-	public Ip(String ipStr)
-	{
+	public Ip(String ipStr) {
+		if (!isIp(ipStr)) {
+			throw new IllegalArgumentException("Illegal IP: " + ipStr);
+		}
 		String[] parts = ipStr.split("\\.");
 		long d1 = Long.parseLong(parts[0]) << 24;
 		long d2 = Long.parseLong(parts[1]) << 16;
@@ -13,13 +24,12 @@ public class Ip implements Comparable<Ip>
 		long d4 = Long.parseLong(parts[3]);
 		value = d1 | d2 | d3 | d4;
 	}
-	public Ip(Long value)
-	{
+
+	public Ip(Long value) {
 		this.value = value;
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		long d4 = value & 255;
 		long d3 = (value >> 8) & 255;
 		long d2 = (value >> 16) & 255;
@@ -27,25 +37,37 @@ public class Ip implements Comparable<Ip>
 		return d1 + "." + d2 + "." + d3 + "." + d4;
 	}
 
-	public Long toLong()
-	{
+	public Long toLong() {
 		return value;
 	}
 
-	public int compareTo(Ip obj)
-	{
+	public int compareTo(Ip obj) {
 		return value.compareTo(obj.toLong());
 	}
-	
+
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Ip)) {
 			return false;
 		}
-		return (compareTo((Ip)obj) == 0);
+		return (compareTo((Ip) obj) == 0);
 	}
-	
+
 	public int hashCode() {
 		return value.intValue();
 	}
-	
+
+	public static boolean isIp(String candidate) {
+		String[] parts = candidate.split("\\.");
+		if (parts.length != 4) {
+			return false;
+		}
+		for (String part : parts) {
+			int value = Integer.parseInt(part);
+			if (value < 0 || value > 255) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
--- trunk/core/src/org/punksearch/ip/IpRange.java
+++ trunk/core/src/org/punksearch/ip/IpRange.java
@@ -1,45 +1,56 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.ip;
 
-public class IpRange
-{
-	private Ip	startIp;
-	private Ip	finishIp;
+public class IpRange {
+	private Ip startIp;
+	private Ip finishIp;
 
-	public IpRange(String strRange)
-	{
+	public IpRange(String strRange) {
+		if (!isIpRange(strRange)) {
+			throw new IllegalArgumentException("Illegal IP range: " + strRange);
+		}
 		String[] parts = strRange.split("-");
-		if (parts.length == 2)
-		{
+		if (parts.length == 2) {
 			startIp = new Ip(parts[0]);
 			finishIp = new Ip(parts[1]);
-		}
-		else
-		{
+		} else {
 			startIp = new Ip(strRange);
 			finishIp = new Ip(strRange);
 		}
 	}
 
-	public Ip getFinishIp()
-	{
+	public Ip getFinishIp() {
 		return finishIp;
 	}
 
-	public Ip getStartIp()
-	{
+	public Ip getStartIp() {
 		return startIp;
 	}
 
-	public String toString()
-	{
-		if (!startIp.equals(finishIp))
-		{
+	public String toString() {
+		if (!startIp.equals(finishIp)) {
 			return startIp + "-" + finishIp;
-		}
-		else
-		{
+		} else {
 			return startIp.toString();
 		}
 	}
-	
+
+	public static boolean isIpRange(String candidate) {
+		String[] parts = candidate.split("-");
+		if (parts.length == 2) {
+			return Ip.isIp(parts[0]) && Ip.isIp(parts[1]);
+		} else {
+			return Ip.isIp(parts[0]);
+		}
+	}
+
 }
