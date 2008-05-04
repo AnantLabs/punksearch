8b13563e955ac459a1fc6b3a0c58121ea18dea2f
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-05-04 14:03:28 +0000

    Header field added

--- trunk/core/src/org/punksearch/common/IndexFields.java
+++ trunk/core/src/org/punksearch/common/IndexFields.java
@@ -11,18 +11,19 @@
 package org.punksearch.common;
 
 public class IndexFields {
-	public static final String EXTENSION           = "Extension";    // avi, mp3, ...
-	public static final String HOST                = "Host";         // [smb|ftp]_10.0.3.2
+	public static final String EXTENSION           = "Extension";   // avi, mp3, ...
+	public static final String HOST                = "Host";        // [smb|ftp]_10.0.3.2
 	public static final String LAST_MODIFIED       = "LastModified"; // timestamp
-	public static final String NAME                = "Name";         // filename without extension
-	public static final String PATH                = "Path";         // /video/best/ghost/
-	public static final String SIZE                = "Size";         // 000705560576
+	public static final String NAME                = "Name";        // filename without extension
+	public static final String PATH                = "Path";        // /video/best/ghost/
+	public static final String SIZE                = "Size";        // 000705560576
 	public static final String DATE                = "Date";
-	
+
 	public static final String INDEXED             = "Indexed";
+	public static final String HEADER              = "Header";
 
 	public static final String DIRECTORY_EXTENSION = "";
-	public static final String SORT_SUFFIX         = "_Sort";        // 000705560576
+	public static final String SORT_SUFFIX         = "_Sort";       // 000705560576
 
 	private IndexFields() {
 	}
