014da1576a99631056c4b4f668c840043933c2d6
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-08-06 10:13:25 +0000

    cleanup, comments

--- trunk/core/src/org/punksearch/common/OnlineChecker.java
+++ trunk/core/src/org/punksearch/common/OnlineChecker.java
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.common;
 
 import java.io.IOException;
@@ -5,24 +15,31 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 
+/**
+ * Tests if some particular host is online. Both SMB and FTP protocols are supported.
+ * 
+ * This is active implementation, no caching is used.
+ * 
+ * Hosts are described like: [protocol]://[ip] or [protocol]_[ip]
+ * 
+ * Examples: smb://10.20.30.40 or smb_10.20.30.40
+ * 
+ * Alternatively isActive() method may be used to test any required port for a host. 
+ * 
+ * @author Yury Soldak (ysoldak@gmail.com)
+ * 
+ */
 public class OnlineChecker {
 
-	private static int     TIMEOUT;
+	public static final String   TIMEOUT_PROPERTY = "org.punksearch.online.timeout";
+
+	private static int           TIMEOUT          = Integer.valueOf(System.getProperty(TIMEOUT_PROPERTY, "5000"));
 	private static final boolean USE_OLD_SMB_PORT = true;
 
 	private static final int     SMB_PORT         = 445;
 	private static final int     SMB_OLD_PORT     = 139;
 	private static final int     FTP_PORT         = 21;
 
-	static {
-		String timeout = System.getProperty("org.punksearch.online.timeout");
-		if (timeout != null) {
-			TIMEOUT = Integer.valueOf(timeout);
-		} else {
-			TIMEOUT = 5000;
-		}
-	}
-	
 	public static boolean isOnline(String host) {
 		if (!(host.startsWith("smb") || host.startsWith("ftp"))) {
 			throw new IllegalArgumentException("Unknown protocol: " + host);
