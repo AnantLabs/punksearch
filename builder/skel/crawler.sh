7516fcedacaa5f793468ea5fec4700bb0e18d88f
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-08-20 20:16:40 +0000

    auto determine PUNKSERACH_HOME

--- builder/skel/crawler.sh
+++ builder/skel/crawler.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+# Only set PUNKSEARCH_HOME if not already set
+[ -z "$PUNKSEARCH_HOME" ] && PUNKSEARCH_HOME=`cd "$PRGDIR/.." ; pwd`
+
 CP=.
-for file in lib/*.jar; do
+for file in $PUNKSEARCH_HOME/lib/*.jar; do
 	CP=$CP:$file
 done;
 
