c9a348b44477369338bba1215702c02c893676fb
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-08-21 14:17:50 +0000

    introduced new index field "Type" with values "DIR" or "FILE" to distinguish directories from files not by extension, but by type stored during crawling

--- core/src/org/punksearch/common/IndexFields.java
+++ core/src/org/punksearch/common/IndexFields.java
@@ -11,19 +11,21 @@
 package org.punksearch.common;
 
 public class IndexFields {
-	public static final String EXTENSION           = "Extension";   // avi, mp3, ...
-	public static final String HOST                = "Host";        // [smb|ftp]_10.0.3.2
-	public static final String LAST_MODIFIED       = "LastModified"; // timestamp
-	public static final String NAME                = "Name";        // filename without extension
-	public static final String PATH                = "Path";        // /video/best/ghost/
-	public static final String SIZE                = "Size";        // 000705560576
-	public static final String DATE                = "Date";
+	public static final String EXTENSION     = "Extension";   // avi, mp3, ...
+	public static final String HOST          = "Host";        // [smb|ftp]_10.0.3.2
+	public static final String LAST_MODIFIED = "LastModified"; // timestamp
+	public static final String NAME          = "Name";        // filename without extension
+	public static final String PATH          = "Path";        // /video/best/ghost/
+	public static final String SIZE          = "Size";        // 000705560576
+	public static final String DATE          = "Date";
+	public static final String TYPE          = "Type";        // DIR or FILE
 
-	public static final String INDEXED             = "Indexed";
-	public static final String HEADER              = "Header";
+	public static final String INDEXED       = "Indexed";
+	public static final String HEADER        = "Header";
 
-	public static final String DIRECTORY_EXTENSION = "";
-	public static final String SORT_SUFFIX         = "_Sort";       // 000705560576
+	public static final String TYPE_DIR      = "DIR";
+	public static final String TYPE_FILE     = "FILE";
+	public static final String SORT_SUFFIX   = "_Sort";       // 000705560576
 
 	private IndexFields() {
 	}
