33b54ac98386925f8a656bdef639b89c04d41de7
ysoldak <ysoldak@008587e4-6633-0410-8dc5-b551423f2b6a>
2008-08-21 12:53:40 +0000

    made "min" inclusive, "max" exclusive

--- core/src/org/punksearch/searcher/filters/FilterFactory.java
+++ core/src/org/punksearch/searcher/filters/FilterFactory.java
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *                                                                         *
+ *   PunkSearch - Searching over LAN                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package org.punksearch.searcher.filters;
 
 import org.apache.lucene.search.Filter;
@@ -5,23 +15,19 @@ import org.punksearch.common.FileType;
 import org.punksearch.common.FileTypes;
 import org.punksearch.common.IndexFields;
 
-public class FilterFactory
-{
+public class FilterFactory {
 
-	public static NumberRangeFilter<Long> createNumberFilter(String fieldName, Long min, Long max)
-	{
-		if (min == null && max == null)
-		{
+	public static NumberRangeFilter<Long> createNumberFilter(String fieldName, Long min, Long max) {
+		if (min == null && max == null) {
 			throw new IllegalArgumentException("Both min and max can't be null at the same time");
 		}
-		if (min != null && max != null && min > max)
-		{
+		if (min != null && max != null && min > max) {
 			throw new IllegalArgumentException("min > max");
 		}
-		
-		boolean includeLower = (min != null);
-		boolean includeUpper = (max != null);
-		
+
+		boolean includeLower = true; // lower bound is inclusive
+		boolean includeUpper = false; // upper bound is exclusive
+
 		NumberRangeFilter<Long> filter = new NumberRangeFilter<Long>(fieldName, min, max, includeLower, includeUpper) {
 			public Long termTextToNumber(String text) {
 				return Long.valueOf(text);
@@ -29,7 +35,7 @@ public class FilterFactory
 		};
 		return filter;
 	}
-	
+
 	public Filter makeSizeFilter(FileTypes types, String typeName) {
 		FileType type = types.get(typeName);
 		Long min = (type == null || type.getMinBytes() <= 0) ? null : type.getMinBytes();
@@ -37,5 +43,5 @@ public class FilterFactory
 		NumberRangeFilter<Long> sizeFilter = createNumberFilter(IndexFields.SIZE, min, max);
 		return sizeFilter;
 	}
-	
+
 }
