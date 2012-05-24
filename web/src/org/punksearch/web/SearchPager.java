2669e240914bf0439812452154f3ca49ed678664
xonixx@gmail.com <xonixx@gmail.com@008587e4-6633-0410-8dc5-b551423f2b6a>
2012-05-24 14:10:59 +0000

    rfct

--- web/src/org/punksearch/web/SearchPager.java
+++ web/src/org/punksearch/web/SearchPager.java
@@ -1,67 +1,60 @@
 package org.punksearch.web;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletRequest;
 
 public class SearchPager {
 
-	private static final int PAGE_LINKS_PER_SIDE = 10;
-	public static final int PAGE_SIZE = 15;
-	
-    public static String makePagesRow(HttpServletRequest request, int all)
-    {
-		int cur = SearchParams.getIntegerValue(request, "first", 0)/PAGE_SIZE;
-    	
-    	if (all <= PAGE_SIZE)
-    		return "";
+    private static final int PAGE_LINKS_PER_SIDE = 10;
+    public static final int PAGE_SIZE = 15;
 
-    	String result = "<div>";
-    	
-    	int maxNum = (all%PAGE_SIZE == 0)? all/PAGE_SIZE : all/PAGE_SIZE + 1; 
-    	//int pageNum = 0;
-    	int startNum = (cur - PAGE_LINKS_PER_SIDE < 0)? 0 : cur - PAGE_LINKS_PER_SIDE;
-    	int stopNum  = (cur + PAGE_LINKS_PER_SIDE + 1 > maxNum)? maxNum : cur + PAGE_LINKS_PER_SIDE + 1;
-    	
-    	String urlPrefix = "?";
-    	for (Object o_key : request.getParameterMap().keySet())
-    	{
-    		String key = (String) o_key;
-    		if (!key.equals("first") && !key.equals("last"))
-    		{
-    				urlPrefix += key + "=" + SearchParams.getStringValue(request, key).replace("+", "%2B") + "&";
-    		}
-    	}
-    	
-    	for (int i = startNum; i < stopNum; i++)
-    	{
-    		if (i == cur)
-    		{
-    			result += makePageLink("", i+1);
-    		}
-    		else
-    		{
-    			String url = urlPrefix + "first=" + (i*PAGE_SIZE) + "&last=" + ((i+1)*PAGE_SIZE-1);
-    			result += makePageLink(url, i+1);
-    		}
-    	}
-     	return result+"</div>";
-    }
-    
-    public static int getPageCount(int all)
-    {
-    	return (all%PAGE_SIZE == 0)? all/PAGE_SIZE : all/PAGE_SIZE + 1;
+    public static String makePagesRow(ServletRequest request, int all) {
+        int cur = SearchParams.getIntegerValue(request, "first", 0) / PAGE_SIZE;
+
+        if (all <= PAGE_SIZE)
+            return "";
+
+        StringBuilder result = new StringBuilder("<div>");
+
+        int maxNum = (all % PAGE_SIZE == 0) ? all / PAGE_SIZE : all / PAGE_SIZE + 1;
+        //int pageNum = 0;
+        int startNum = (cur - PAGE_LINKS_PER_SIDE < 0) ? 0 : cur - PAGE_LINKS_PER_SIDE;
+        int stopNum = (cur + PAGE_LINKS_PER_SIDE + 1 > maxNum) ? maxNum : cur + PAGE_LINKS_PER_SIDE + 1;
+
+        StringBuilder urlPrefix = new StringBuilder("?");
+        for (Object o_key : request.getParameterMap().keySet()) {
+            String key = (String) o_key;
+
+            if (!key.equals("first") && !key.equals("last")) {
+                urlPrefix.append(key)
+                        .append("=")
+                        .append(SearchParams.getStringValue(request, key).replace("+", "%2B"))
+                        .append("&");
+            }
+        }
+
+        for (int i = startNum; i < stopNum; i++) {
+            if (i == cur) {
+                result.append(makePageLink("", i + 1));
+            } else {
+                String url = urlPrefix.toString() + "first=" + (i * PAGE_SIZE) + "&last=" + ((i + 1) * PAGE_SIZE - 1);
+                result.append(makePageLink(url, i + 1));
+            }
+        }
+
+        result.append("</div>");
+
+        return result.toString();
     }
 
-    private static String makePageLink(String url, int num)
-    {
-    	if (url.length() != 0)
-    	{
-    		return "<a href=\""+url+"\" class=\"pageNumStyle\">" + num + "</a>";
-    	}
-    	else
-    	{
-    		return "<a class=\"pageCurrNumStyle\">" + num + "</a>";
-    	}
-    }    
-	
-	
+    public static int getPageCount(int all) {
+        return (all % PAGE_SIZE == 0) ? all / PAGE_SIZE : all / PAGE_SIZE + 1;
+    }
+
+    private static String makePageLink(String url, int num) {
+        if (url.length() != 0) {
+            return "<a href=\"" + url + "\" class=\"pageNumStyle\">" + num + "</a>";
+        } else {
+            return "<a class=\"pageCurrNumStyle\">" + num + "</a>";
+        }
+    }
 }
--- web/src/org/punksearch/web/SearchParams.java
+++ web/src/org/punksearch/web/SearchParams.java
@@ -10,134 +10,131 @@
  ***************************************************************************/
 package org.punksearch.web;
 
-import java.io.UnsupportedEncodingException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.ServletRequest;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * @author Yury Soldak (ysoldak@gmail.com)
  */
 public class SearchParams {
-	private static Log       __log       = LogFactory.getLog(SearchParams.class);
+    private static Log log = LogFactory.getLog(SearchParams.class);
 
-	private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
 
-	public String               type        = "everything";
-	public String               query       = null;
+    public String type = "everything";
+    public String query = null;
 
-	public String               dir         = null;
-	public String               file        = null;
-	public String               ext         = null;
+    public String dir = null;
+    public String file = null;
+    public String ext = null;
 
-	public Long                 minSize     = null;
-	public Long                 maxSize     = null;
+    public Long minSize = null;
+    public Long maxSize = null;
 
-	public Long                 fromDate    = null;
-	public Long                 toDate      = null;
+    public Long fromDate = null;
+    public Long toDate = null;
 
-	public Integer              first       = 0;
-	public Integer              last        = SearchPager.PAGE_SIZE - 1;
+    public Integer first = 0;
+    public Integer last = SearchPager.PAGE_SIZE - 1;
 
-	public SearchParams(HttpServletRequest request) {
-		this.type = getStringValue(request, "type");
-		if (this.type.length() == 0)
-			this.type = "everything";
+    public SearchParams(ServletRequest request) {
+        this.type = getStringValue(request, "type");
+        if (this.type.length() == 0)
+            this.type = "everything";
 
-		/* first & last */
-		this.first = getIntegerValue(request, "first", 0);
-		this.last = getIntegerValue(request, "last", SearchPager.PAGE_SIZE - 1);
+        /* first & last */
+        this.first = getIntegerValue(request, "first", 0);
+        this.last = getIntegerValue(request, "last", SearchPager.PAGE_SIZE - 1);
 
-		if (type.equals("advanced")) {
-			/* dir & file & ext */
-			this.dir = getStringValue(request, "dir");
-			this.file = getStringValue(request, "file");
-			this.ext = getStringValue(request, "ext");
+        if (type.equals("advanced")) {
+            /* dir & file & ext */
+            this.dir = getStringValue(request, "dir");
+            this.file = getStringValue(request, "file");
+            this.ext = getStringValue(request, "ext");
 
-			/* maxSize & minSize */
-			this.minSize = (Long) Math.round(getDoubleValue(request, "minSize", 0.0) * 1024 * 1024);
-			this.maxSize = (Long) Math.round(getDoubleValue(request, "maxSize", 0.0) * 1024 * 1024);
-			if (minSize == 0)
-				minSize = null;
-			if (maxSize == 0)
-				maxSize = null;
-			if (this.minSize != null && this.maxSize != null && this.minSize > this.maxSize) {
-				this.minSize = this.maxSize = null;
-			}
+            /* maxSize & minSize */
+            this.minSize = (Long) Math.round(getDoubleValue(request, "minSize", 0.0) * 1024 * 1024);
+            this.maxSize = (Long) Math.round(getDoubleValue(request, "maxSize", 0.0) * 1024 * 1024);
+            if (minSize == 0)
+                minSize = null;
+            if (maxSize == 0)
+                maxSize = null;
+            if (this.minSize != null && this.maxSize != null && this.minSize > this.maxSize) {
+                this.minSize = this.maxSize = null;
+            }
 
-			/* fromDate & toDate */
-			DateFormat fmt = new SimpleDateFormat(DATE_FORMAT);
-			try {
-				this.fromDate = fmt.parse(getStringValue(request, "fromDate")).getTime();
-			} catch (ParseException pe) {
-				// __log.warning(pe.getMessage());
-			}
-			try {
-				this.toDate = fmt.parse(getStringValue(request, "toDate")).getTime();
-			} catch (ParseException pe) {
-				// __log.warning(pe.getMessage());
-			}
-			if (this.fromDate != null && this.toDate != null && this.fromDate > this.toDate) {
-				this.fromDate = this.toDate = null;
-			}
-		} else {
-			this.query = getStringValue(request, "query");
-		}
-	}
+            /* fromDate & toDate */
+            DateFormat fmt = new SimpleDateFormat(DATE_FORMAT);
+            try {
+                this.fromDate = fmt.parse(getStringValue(request, "fromDate")).getTime();
+            } catch (ParseException pe) {
+                log.warn("problem parsing fromDate: " + pe.getMessage());
+            }
+            try {
+                this.toDate = fmt.parse(getStringValue(request, "toDate")).getTime();
+            } catch (ParseException pe) {
+                log.warn("problem parsing toDate: " + pe.getMessage());
+            }
+            if (this.fromDate != null && this.toDate != null && this.fromDate > this.toDate) {
+                this.fromDate = this.toDate = null;
+            }
+        } else {
+            this.query = getStringValue(request, "query");
+        }
+    }
 
-	public static String getStringValue(HttpServletRequest request, String paramName) {
-		String paramValue = request.getParameter(paramName);
-		if (paramValue == null)
-			return "";
+    public static String getStringValue(ServletRequest request, String paramName) {
+        String paramValue = request.getParameter(paramName);
+        if (paramValue == null)
+            return "";
 
 //        OMG WTF??? this prevents russian queries!
 /*		try {
 			paramValue = new String(paramValue.getBytes("ISO-8859-1"), "UTF-8");
 		} catch (UnsupportedEncodingException uee) {
-			__log.warn(uee.getMessage());
+			log.warn(uee.getMessage());
 		}*/
 
-		return paramValue;
-	}
+        return paramValue;
+    }
 
-	public static Integer getIntegerValue(HttpServletRequest request, String paramName, Integer byDefault) {
-		String paramValue = request.getParameter(paramName);
-		if (paramValue == null || paramValue.equals(""))
-			return byDefault;
+    public static Integer getIntegerValue(ServletRequest request, String paramName, Integer byDefault) {
+        String paramValue = request.getParameter(paramName);
+        if (paramValue == null || paramValue.equals(""))
+            return byDefault;
 
-		return Integer.valueOf(paramValue);
+        return Integer.valueOf(paramValue);
 
-	}
+    }
 
-	public static Double getDoubleValue(HttpServletRequest request, String paramName, Double byDefault) {
-		String paramValue = request.getParameter(paramName);
-		if (paramValue == null || paramValue.equals(""))
-			return byDefault;
+    public static Double getDoubleValue(ServletRequest request, String paramName, Double byDefault) {
+        String paramValue = request.getParameter(paramName);
+        if (paramValue == null || paramValue.equals(""))
+            return byDefault;
 
-		return Double.valueOf(paramValue);
+        return Double.valueOf(paramValue);
 
-	}
+    }
 
-	public static Boolean getBooleanValue(HttpServletRequest request, String paramName, Boolean byDefault) {
-		String paramValue = request.getParameter(paramName);
-		if (paramValue == null || paramValue.equals(""))
-			return byDefault;
+    public static Boolean getBooleanValue(ServletRequest request, String paramName, Boolean byDefault) {
+        String paramValue = request.getParameter(paramName);
+        if (paramValue == null || paramValue.equals(""))
+            return byDefault;
 
-		return paramValue.equals("true") || paramValue.equals("on");
+        return paramValue.equals("true") || paramValue.equals("on");
 
-	}
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getQuery() {
-		return query;
-	}
-
+    public String getQuery() {
+        return query;
+    }
 }
