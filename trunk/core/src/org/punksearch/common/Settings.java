e37ea17dcd8182401aaf1b7371aa8ed192618fc4
xonixx@gmail.com <xonixx@gmail.com@008587e4-6633-0410-8dc5-b551423f2b6a>
2012-05-24 14:53:59 +0000

    rfct Settings

---
+++ trunk/core/src/org/punksearch/common/Settings.java
@@ -0,0 +1,55 @@
+package org.punksearch.common;
+
+/**
+ * User: gubarkov
+ * Date: 24.05.12
+ * Time: 18:46
+ */
+public class Settings {
+    /*
+   Access methods
+    */
+    public static int getInt(String key) {
+        return getInt(key, 0);
+    }
+
+    public static int getInt(String key, int defaultVal) {
+        return Integer.getInteger(key, defaultVal);
+    }
+
+    public static float getFloat(String key) {
+        return getFloat(key, 0F);
+    }
+
+    public static float getFloat(String key, float defaultVal) {
+        final String propVal = System.getProperty(key);
+
+        if (propVal == null) {
+            return defaultVal;
+        }
+
+        return Float.parseFloat(propVal);
+    }
+
+    public static long getLong(String key) {
+        return getLong(key, 0L);
+    }
+
+    public static long getLong(String key, long defaultVal) {
+        return Long.getLong(key, defaultVal);
+    }
+
+    public static boolean getBool(String key) {
+        return getBool(key, false);
+    }
+
+    public static boolean getBool(String key, boolean defaultVal) {
+        final String propVal = System.getProperty(key);
+
+        if (propVal == null) {
+            return defaultVal;
+        }
+
+        return Boolean.parseBoolean(propVal);
+    }
+}
