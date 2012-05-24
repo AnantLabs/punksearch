b26c0c4111babffd4faec153df06824ffc7a29f1
xonixx@gmail.com <xonixx@gmail.com@008587e4-6633-0410-8dc5-b551423f2b6a>
2012-05-24 13:22:41 +0000

    fix

--- trunk/core/test/org/punksearch/experiments/ResolveHostTests.java
+++ trunk/core/test/org/punksearch/experiments/ResolveHostTests.java
@@ -1,6 +1,8 @@
 package org.punksearch.experiments;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.punksearch.hosts_resolver.HostnameByIpComputable;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -17,4 +19,14 @@ public class ResolveHostTests {
         String host = addr.getHostName();
         System.out.println(host);
     }
+
+    @Test
+    public void resolveHostTest() throws InterruptedException {
+        HostnameByIpComputable hostnameByIpComputable = new HostnameByIpComputable();
+
+        Assert.assertEquals("localhost", hostnameByIpComputable.compute("127.0.0.1"));
+        Assert.assertEquals("google-public-dns-a.google.com", hostnameByIpComputable.compute("8.8.8.8"));
+        Assert.assertEquals(null, hostnameByIpComputable.compute("999.999.999.999"));
+    }
+
 }
