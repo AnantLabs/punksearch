0db7834381bf52997047172aca5d15e194ddf802
xonixx@gmail.com <xonixx@gmail.com@008587e4-6633-0410-8dc5-b551423f2b6a>
2012-06-01 13:50:58 +0000

    ip /ip range impr (constructing ip range by "n1.n2.*" added)

--- core/test/org/punksearch/ip/IpRangeTest.java
+++ core/test/org/punksearch/ip/IpRangeTest.java
@@ -24,5 +24,25 @@ public class IpRangeTest extends TestCase {
         assertEquals(new Ip("1.2.0.0"), ipr.getStartIp());
         assertEquals(new Ip("1.2.255.255"), ipr.getFinishIp());
         assertEquals(new IpRange("1.2.0.0-1.2.255.255"), ipr);
+
+        IpRange allIps = new IpRange("*");
+        assertEquals(new IpRange("0.0.0.0-255.255.255.255"), allIps);
+    }
+
+    public void testWildcardRangeValidity() {
+        new IpRange("*");
+        new IpRange("1.*");
+        new IpRange("1.2.*");
+        new IpRange("1.2.3.*");
+        try {
+            new IpRange("1.2.3.4.*");
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new IpRange("1.2.3.4.5.*");
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
     }
 }
