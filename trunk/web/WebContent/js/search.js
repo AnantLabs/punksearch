bcf130b1796716d3397cb046cce919709418a57e
xonixx@gmail.com <xonixx@gmail.com@008587e4-6633-0410-8dc5-b551423f2b6a>
2012-06-04 23:18:57 +0000

    scanned.jsp fixes

--- trunk/web/WebContent/js/search.js
+++ trunk/web/WebContent/js/search.js
@@ -1,23 +1,23 @@
-function changeSearchType(type)
-{
-	var params = "?type="+type;
-    
-	var queryElement = document.forms["searchForm"].elements["query"];
-	if ((queryElement != null)&&(queryElement.value.length != 0)) {
-		params += "&query=" + queryElement.value.replace(/\+/g, "%2B");
-	}                               
-	/*
-	var offlineElement = document.forms["searchForm"].elements["showoffline"];
-	if ((offlineElement != null)&&(offlineElement.checked)) {
-		params += "&showoffline=on";
-	}       
-	*/
-	window.location = params;
+function changeSearchType(type) {
+    var params = "?type=" + type;
+
+    var queryElement = document.forms["searchForm"].elements["query"];
+    if ((queryElement != null) && (queryElement.value.length != 0)) {
+        params += "&query=" + queryElement.value.replace(/\+/g, "%2B");
+    }
+    /*
+     var offlineElement = document.forms["searchForm"].elements["showoffline"];
+     if ((offlineElement != null)&&(offlineElement.checked)) {
+     params += "&showoffline=on";
+     }
+     */
+    window.location = params;
 }
 
-function setFocus(type)
-{
-	var id = (type != null && type == 'advanced')? 'dir' : 'query';
-	var input = document.getElementById(id);
-	input.focus();
+function setFocus(type) {
+    var id = (type != null && type == 'advanced') ? 'dir' : 'query';
+    var input = document.getElementById(id);
+    if (input) {
+        input.focus();
+    }
 }
